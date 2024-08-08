8a27e2914c54f07180fa41b1fcc461aae635b3e1
Erlend Wiklem <admin@wiklem.no>
2024-08-08 11:14:57 +0200

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import '@navikt/ds-css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import { ApplikasjonContextProvider } from './ApplikasjonContext';
 import Header from './header/Header';
+import { getMiljø, Miljø } from './util/miljø';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +17,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isProd = getMiljø() === Miljø.ProdGcp;
+  const scriptSrc = isProd
+    ? 'https://cdn.nav.no/personoversikt/internarbeidsflate-decorator-v3/prod/latest/dist/bundle.js'
+    : 'https://cdn.nav.no/personoversikt/internarbeidsflate-decorator-v3/dev/latest/dist/bundle.js';
+  const linkHref = isProd
+    ? 'https://cdn.nav.no/personoversikt/internarbeidsflate-decorator-v3/prod/latest/dist/index.css'
+    : 'https://cdn.nav.no/personoversikt/internarbeidsflate-decorator-v3/dev/latest/dist/index.css';
+
   return (
     <html lang='no'>
+      <Script src={scriptSrc}></Script>
+      <link rel='stylesheet' href={linkHref} />
       <body className={inter.className}>
         <ApplikasjonContextProvider>
           <Header />
