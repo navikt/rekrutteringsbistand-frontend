314c4745d61ba1f386bb2396ad9d089577f84a67
Erlend Wiklem <admin@wiklem.no>
2024-08-09 14:19:49 +0200

    WIP

--- app/header/Modiadekoratør.tsx
+++ app/header/Modiadekoratør.tsx
@@ -1,3 +1,4 @@
+import { InternalHeader, Spacer } from '@navikt/ds-react';
 import NAVSPA from '@navikt/navspa';
 import { FunctionComponent } from 'react';
 import { isLocal } from '../util/env';
@@ -9,11 +10,20 @@ const InternflateDecorator = NAVSPA.importer<DecoratorProps>(
 
 const Modiadekoratør: FunctionComponent = () => {
   if (isLocal) {
-    return <div> Ingen header lokalt gitt </div>;
+    return (
+      <InternalHeader>
+        <InternalHeader.Title as='h1'>
+          Rekrutteringsbistand - DEV
+        </InternalHeader.Title>
+        <Spacer />
+        <InternalHeader.User name='Developer' />
+      </InternalHeader>
+    );
   }
   return (
     <InternflateDecorator
       useProxy
+      proxy='https://rekrutteringsbistand-next.intern.dev.nav.no/'
       onEnhetChanged={function (enhetId?: string | null, enhet?: Enhet): void {
         console.log('Function not implemented.');
       }}
