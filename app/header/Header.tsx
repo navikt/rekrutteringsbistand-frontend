ae1be80294ada7c10d2e1113028953eff457e649
Erlend Wiklem <admin@wiklem.no>
2024-08-09 14:34:04 +0200

    WIP

--- app/header/Header.tsx
+++ app/header/Header.tsx
@@ -3,11 +3,7 @@ import * as React from 'react';
 import Modiadekoratør from './Modiadekoratør';
 import { Navigeringsmeny } from './Navigasjon';
 
-export interface IHeader {
-  children?: React.ReactNode | undefined;
-}
-
-const Header: React.FC<IHeader> = ({ children }) => {
+const Header: React.FC = () => {
   return (
     <React.Fragment>
       <Modiadekoratør />
