bd92bb913a5b3919c609d9a7600de4ff4e66df2d
Erlend Wiklem <admin@wiklem.no>
2024-08-20 10:50:11 +0200

    WIP

--- app/util/env.ts
+++ app/util/env.ts
@@ -1,6 +1,15 @@
 export const isLocal = process.env.NAIS_CLUSTER_NAME === 'local';
+
+const onPremCLuster = () => {
+  if (process.env.NAIS_CLUSTER_NAME === 'prod-gcp') {
+    return 'prod-fss';
+  } else {
+    return 'dev-fss';
+  }
+};
+
 export const getCluster = (onPrem?: boolean) => {
   const cluster = process.env.NAIS_CLUSTER_NAME;
-  const clusterOnPrem = cluster === 'prod-gcp' && onPrem ? 'prod-fss' : cluster;
+  const clusterOnPrem = onPrem ? onPremCLuster() : cluster;
   return clusterOnPrem;
 };
