207632c780ea2dabf64b5b6482660a7967989b35
Erlend Wiklem <admin@wiklem.no>
2024-08-27 11:49:47 +0200

    WIP

--- app/api/api-routes.ts
+++ app/api/api-routes.ts
@@ -18,7 +18,7 @@ export const ModiaDecoratorAPI: Iroute = {
 };
 
 export const StatistikkAPI: Iroute = {
-  api_route: "/rekrutteringsbistand-statistikk-ap/api/statistikk",
+  api_route: "/rekrutteringsbistand-statistikk-apo/api/statistikk",
   api_url: process.env.STATISTIKK_API ?? "",
   internUrl: "/api/statistikk",
   scope: `api://${fss}.toi.rekrutteringsbistand-statistikk-api/.default`,
