4b526493ae0a4cdc5faa551f70f9d05ef500cbc4
Erlend Wiklem <admin@wiklem.no>
2024-08-27 21:43:09 +0200

    WIP

--- app/kandidatsok/page.tsx
+++ app/kandidatsok/page.tsx
@@ -6,7 +6,10 @@ const Page: React.FC = () => {
   const { isLoading, error, data } = useMinebrukere();
   return (
     <React.Fragment>
-      Hello <ul>{data && data.kandidater.map((b) => <li>{b.fornavn}</li>)}</ul>
+      Hello{" "}
+      <ul>
+        {data && data.kandidater.map((b, i) => <li key={i}>{b.fornavn}</li>)}
+      </ul>
     </React.Fragment>
   );
 };
