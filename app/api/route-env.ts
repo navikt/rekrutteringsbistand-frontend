b056412f47e839e2822c201325b986a318fb3d11
Erlend Wiklem <admin@wiklem.no>
2024-08-27 09:23:40 +0200

    WIP

--- app/api/route-env.ts
+++ app/api/route-env.ts
@@ -5,30 +5,35 @@ const fss = getCluster(true);
 
 interface Iroute {
   api_url: string;
+  api_route: string;
   scope: string;
   internUrl: string;
 }
 
 export const ModiaDecoratorAPI: Iroute = {
+  api_route: "",
   api_url: process.env.MODIA_CONTEXT_HOLDER_API ?? "",
-  scope: `api://${gcp}.personoversikt.modiacontextholder/.default`,
   internUrl: "",
+  scope: `api://${gcp}.personoversikt.modiacontextholder/.default`,
 };
 
 export const StatistikkAPI: Iroute = {
+  api_route: "/statistikk",
   api_url: process.env.STATISTIKK_API_UR ?? "",
-  scope: `api://${fss}.toi.rekrutteringsbistand-statistikk-api/.default`,
   internUrl: "/api/statistikk",
+  scope: `api://${fss}.toi.rekrutteringsbistand-statistikk-api/.default`,
 };
 
 export const ForespørselDelingAvCvAPI: Iroute = {
-  scope: `api://${fss}.arbeidsgiver-inkludering.foresporsel-om-deling-av-cv-api/.default`,
+  api_route: "/statistikk/foresporsel-om-deling-av-cv",
   api_url: process.env.FORESPORSEL_OM_DELING_AV_CV_API ?? "",
   internUrl: "/api/foresporsel-om-deling-av-cv/statistikk",
+  scope: `api://${fss}.arbeidsgiver-inkludering.foresporsel-om-deling-av-cv-api/.default`,
 };
 
 export const StillingAPI: Iroute = {
+  api_route: "",
   api_url: process.env.STILLING_API_URL ?? "",
-  scope: `api://${gcp}.toi.rekrutteringsbistand-stilling-api/.default`,
   internUrl: "/api/stilling",
+  scope: `api://${gcp}.toi.rekrutteringsbistand-stilling-api/.default`,
 };
