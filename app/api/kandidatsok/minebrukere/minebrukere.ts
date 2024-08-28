31ded7ced52fd22a427e80cf5ffb012bfbd45a16
Erlend Wiklem <admin@wiklem.no>
2024-08-28 22:01:20 +0200

    WIP

--- app/api/kandidatsok/minebrukere/minebrukere.ts
+++ app/api/kandidatsok/minebrukere/minebrukere.ts
@@ -6,13 +6,14 @@ import useSWRImmutable from 'swr/immutable';
 import { postApiWithSchema } from '../../fetcher';
 import { kandidatSokSchema } from '../types';
 
-const minebrukereEndepunkt = '/api/kandidatsok/minebrukere';
+const minebrukereEndepunkt =
+  '/api/kandidatsok/minebrukere?side=1&sortering=nyeste';
 
-export const useMinebrukere = (payload : any) =>
+export const useMinebrukere = (payload: any) =>
   useSWRImmutable(
     {
       url: minebrukereEndepunkt,
-      body: payload
+      body: payload,
     },
     (data) => {
       return postApiWithSchema(kandidatSokSchema)(data);
