0818eccba9351af60b17b461bddfccd9739080f3
Erlend Wiklem <admin@wiklem.no>
2024-09-01 21:36:14 +0200

    WIP

--- app/stillingssok/page.tsx
+++ app/stillingssok/page.tsx
@@ -1,6 +1,6 @@
+import '../globals.css';
 import StillingsSøk from './StillingsSøk';
 import { StillingsSøkProvider } from './StillingsSøkContext';
-
 export default function StillingsSøkIndex() {
   return (
     <StillingsSøkProvider>
