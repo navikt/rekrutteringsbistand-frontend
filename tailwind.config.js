1e8faff08fe96bf635a46b3cbc01ec3c0968d17b
Erlend Wiklem <admin@wiklem.no>
2024-09-01 18:20:58 +0200

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   presets: [require('@navikt/ds-tailwind')],
   content: [
+    './app/(*)/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
