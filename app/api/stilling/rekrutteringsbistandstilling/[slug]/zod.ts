e7c132af8496e5011df993dd17cc3f70d4c7ed20
Erlend Wiklem <admin@wiklem.no>
2024-09-06 11:42:41 +0200

    WIP

--- app/api/stilling/rekrutteringsbistandstilling/[slug]/zod.ts
+++ app/api/stilling/rekrutteringsbistandstilling/[slug]/zod.ts
@@ -19,14 +19,16 @@ export const stillingSchema = z.object({
     updatedBy: z.string(),
     title: z.string(),
     status: z.string(),
-    administration: z.object({
-      id: z.number(),
-      status: z.string(),
-      comments: z.string().nullable(),
-      reportee: z.string(),
-      remarks: z.array(z.unknown()),
-      navIdent: z.string(),
-    }),
+    administration: z
+      .object({
+        id: z.number(),
+        status: z.string(),
+        comments: z.string().nullable(),
+        reportee: z.string(),
+        remarks: z.array(z.unknown()),
+        navIdent: z.string().nullable(),
+      })
+      .nullable(),
     mediaList: z.array(z.unknown()),
     contactList: z.array(z.unknown()),
     privacy: z.string(),
@@ -35,31 +37,18 @@ export const stillingSchema = z.object({
     reference: z.string(),
     published: z.string(),
     expires: z.string(),
-    employer: z.object({
-      id: z.number(),
-      uuid: z.string(),
-      created: z.string(),
-      createdBy: z.string(),
-      updated: z.string(),
-      updatedBy: z.string(),
-      mediaList: z.array(z.unknown()),
-      contactList: z.array(z.unknown()),
-      location: z
-        .object({
-          address: z.string(),
-          postalCode: z.string(),
-          county: z.string().nullable(),
-          municipal: z.string().nullable(),
-          municipalCode: z.string().nullable(),
-          city: z.string(),
-          country: z.string(),
-          latitude: z.string().nullable(),
-          longitude: z.string().nullable(),
-        })
-        .nullable(),
-      locationList: z
-        .array(
-          z.object({
+    employer: z
+      .object({
+        id: z.number(),
+        uuid: z.string(),
+        created: z.string(),
+        createdBy: z.string(),
+        updated: z.string(),
+        updatedBy: z.string(),
+        mediaList: z.array(z.unknown()),
+        contactList: z.array(z.unknown()),
+        location: z
+          .object({
             address: z.string(),
             postalCode: z.string(),
             county: z.string().nullable(),
@@ -69,20 +58,47 @@ export const stillingSchema = z.object({
             country: z.string(),
             latitude: z.string().nullable(),
             longitude: z.string().nullable(),
-          }),
-        )
-        .nullable(),
-      properties: z.object({ nace2: z.string() }),
-      name: z.string(),
-      orgnr: z.string(),
-      status: z.string(),
-      parentOrgnr: z.string(),
-      publicName: z.string(),
-      deactivated: z.string().nullable(),
-      orgform: z.string(),
-      employees: z.number(),
-    }),
-    location: z.string().nullable(),
+          })
+          .nullable(),
+        locationList: z
+          .array(
+            z.object({
+              address: z.string(),
+              postalCode: z.string(),
+              county: z.string().nullable(),
+              municipal: z.string().nullable(),
+              municipalCode: z.string().nullable(),
+              city: z.string(),
+              country: z.string(),
+              latitude: z.string().nullable(),
+              longitude: z.string().nullable(),
+            }),
+          )
+          .nullable(),
+        properties: z.object({ nace2: z.string() }),
+        name: z.string(),
+        orgnr: z.string(),
+        status: z.string(),
+        parentOrgnr: z.string(),
+        publicName: z.string(),
+        deactivated: z.string().nullable(),
+        orgform: z.string(),
+        employees: z.number(),
+      })
+      .nullable(),
+    location: z
+      .object({
+        address: z.null(),
+        postalCode: z.null(),
+        county: z.null(),
+        municipal: z.null(),
+        municipalCode: z.null(),
+        city: z.null(),
+        country: z.string(),
+        latitude: z.null(),
+        longitude: z.null(),
+      })
+      .nullable(),
     locationList: z.array(z.unknown()),
     categoryList: z.array(z.unknown()),
     properties: z.object({}),
