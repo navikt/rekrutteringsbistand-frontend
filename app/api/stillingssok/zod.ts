f6fcd67084a3ef4f1065cf4b964bc503c8444a4e
Erlend Wiklem <admin@wiklem.no>
2024-09-10 15:14:33 +0200

    WIP

--- app/api/stillingssok/zod.ts
+++ app/api/stillingssok/zod.ts
@@ -57,7 +57,7 @@ export const propertiesSchema = z
     classification_styrk08_score: z.number().optional().nullable(),
     employerdescription: z.string().optional().nullable(),
     adtext: z.string(),
-    classification_styrk08_code: z.number().optional().nullable(),
+    classification_styrk08_code: z.string().optional().nullable(),
     sourceurl: z.string().optional().nullable(),
     searchtags: z.array(searchtagSchema).optional().nullable(),
     applicationurl: z.string().optional().nullable(),
