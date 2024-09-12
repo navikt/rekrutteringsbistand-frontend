34f25b1631685357186b83bb2615c609402bb8f5
Erlend Wiklem <admin@wiklem.no>
2024-09-12 12:32:46 +0200

    Playwright

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:1337/');
   await expect(page.getByRole('tab', { name: 'Oversikt' })).toBeVisible();
 });
