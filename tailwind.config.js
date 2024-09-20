7db3e06df70c4d36863d78c60f7d5bc70c3547b9
Erlend Wiklem <admin@wiklem.no>
2024-09-20 21:08:40 +0200

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx,css}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@navikt/ds-react/**/*.{js,jsx,ts,tsx}',
   ],
   // https://cdn.jsdelivr.net/npm/@navikt/ds-tailwind/tailwind.config.js
   theme: {
