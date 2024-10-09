a3082f65ccdbf8e9719b61e8f450144450fed025
Erlend Wiklem <admin@wiklem.no>
2024-10-09 09:03:46 +0200

    wip

--- app/kandidat-sok/layout.tsx
+++ app/kandidat-sok/layout.tsx
@@ -3,20 +3,18 @@ import SideLayout from '../../components/layout/SideLayout';
 import SideTopBanner from '../../components/layout/SideTopBanner';
 import Piktogram from './components/icons/finn-kandidater.svg';
 import KandidatSøkSidebar from './components/KandidatSøkSidebar';
-import { KandidatSøkProvider } from './KandidatSøkContext';
+
 export default function KandidatSokLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <KandidatSøkProvider>
-      <SideLayout
-        banner={<SideTopBanner tittel='Kandidatsøk' ikon={<Piktogram />} />}
-        sidepanel={<KandidatSøkSidebar />}
-      >
-        {children}
-      </SideLayout>
-    </KandidatSøkProvider>
+    <SideLayout
+      banner={<SideTopBanner tittel='Kandidatsøk' ikon={<Piktogram />} />}
+      sidepanel={<KandidatSøkSidebar />}
+    >
+      {children}
+    </SideLayout>
   );
 }
