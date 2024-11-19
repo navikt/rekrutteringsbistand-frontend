57afcd0007aa28ef464a9f80a21959879c361f00
Erlend Wiklem <admin@wiklem.no>
2024-11-19 10:24:22 +0100

    Test middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,22 +1,17 @@
+import { getToken } from '@navikt/oasis';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { isLocal } from '../util/env';
 
-/**
- * This middleware is purely a hack to be able to provide the react server component auth
- * with the current path. This is needed to be able to redirect to the correct page after
- * login.
- */
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   const requestUrl: URL = new URL(request.url);
-  const requestHeaders = new Headers(request.headers);
+  const requestHeaders = await headers();
   requestHeaders.set('x-path', requestUrl.pathname + requestUrl.search);
-  if (requestHeaders.get('authorization') == null && !isLocal) {
-    const redirectUrl = request.nextUrl.clone();
-    redirectUrl.pathname = `/oauth2/login`;
-    redirectUrl.searchParams.set('redirect', requestUrl.pathname);
 
-    return NextResponse.redirect(redirectUrl);
+  const token = getToken(requestHeaders);
+  if (!token) {
+    redirect(`/oauth2/login?redirect=${requestUrl.pathname}`);
   }
 
   return NextResponse.next({
