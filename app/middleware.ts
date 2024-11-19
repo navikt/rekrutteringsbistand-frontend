1affaea5a98d578c0ebdb18ce5178a4e752bb130
Erlend Wiklem <admin@wiklem.no>
2024-11-19 11:24:22 +0100

    redirect hvis token mangler

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,17 +1,17 @@
-import { getToken } from '@navikt/oasis';
-import { headers } from 'next/headers';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import { isLocal } from '../util/env';
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   const requestUrl: URL = new URL(request.url);
-  const requestHeaders = await headers();
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-path', requestUrl.pathname + requestUrl.search);
+  if (requestHeaders.get('authorization') == null && !isLocal) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = `/oauth2/login`;
+    redirectUrl.searchParams.set('redirect', requestUrl.pathname);
 
-  const token = getToken(requestHeaders);
-  if (!token) {
-    return NextResponse.redirect(
-      new URL(`/oauth2/login?redirect=${requestUrl.pathname}`, request.url),
-    );
+    return NextResponse.redirect(redirectUrl);
   }
 
   return NextResponse.next({
@@ -22,5 +22,6 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)', '/api/bruker'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  missing: [{ type: 'header', key: 'x-path' }],
 };
