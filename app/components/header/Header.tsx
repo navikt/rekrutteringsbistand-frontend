01d39f0a306f4153b79f86719648bd327a587687
Erlend Wiklem <admin@wiklem.no>
2024-11-20 22:31:01 +0100

    Feil ved lasting av modiadekoratør

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,25 +1,16 @@
 'use client';
-import dynamic from 'next/dynamic';
 import * as React from 'react';
-import { Suspense } from 'react';
-// import { isLocal } from '../../../util/env';
-// import DevDekoratør from '../dev/DevDekoratør';
+import { isLocal } from '../../../util/env';
+import DevDekoratør from '../dev/DevDekoratør';
+import Modiadekoratør from './components/modia/Modiadekoratør';
 import { Navigeringsmeny } from './components/navigasjon/Navigasjon';
-
-const ProdDekoratør = dynamic(
-  () => import('./components/modia/Modiadekoratør'),
-  {
-    ssr: false,
-    loading: () => <div>Laster Modia dekoratør...</div>,
-  },
-);
 
 const Header: React.FC = () => {
   return (
-    <Suspense fallback={<div>Laster Modia dekoratør...</div>}>
-      {/* {isLocal ? <DevDekoratør /> : <ProdDekoratør />} */}
+    <>
+      {isLocal ? <DevDekoratør /> : <Modiadekoratør />}
       <Navigeringsmeny />
-    </Suspense>
+    </>
   );
 };
 
--- app/components/header/components/modia/Modiadekoratør.tsx
+++ app/components/header/components/modia/Modiadekoratør.tsx
@@ -1,12 +1,18 @@
+'use client';
 import NAVSPA from '@navikt/navspa';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { getMiljø, Miljø } from '../../../../../util/miljø';
 import { useApplikasjonContext } from '../../../../ApplikasjonContext';
 import { DecoratorProps, Enhet } from './Interndekoratør';
 
-const InternflateDecorator = NAVSPA.importer<DecoratorProps>(
-  'internarbeidsflate-decorator-v3',
-);
+const getDecorator = () => {
+  try {
+    return NAVSPA.importer<DecoratorProps>('internarbeidsflate-decorator-v3');
+  } catch (error) {
+    console.error('Klarte ikke å importere dekoratør:', error);
+    return null;
+  }
+};
 
 const proxyUrl =
   getMiljø() === Miljø.ProdGcp
@@ -17,8 +23,19 @@ const miljo = getMiljø() === Miljø.ProdGcp ? 'prod' : 'q0';
 
 const Modiadekoratør: FunctionComponent = () => {
   const { setValgtNavKontor } = useApplikasjonContext();
+  const [Decorator, setDecorator] = useState<any>(null);
+
+  useEffect(() => {
+    const InternflateDecorator = getDecorator();
+    setDecorator(InternflateDecorator);
+  }, []);
+
+  if (!Decorator) {
+    return <div>Loading decorator...</div>;
+  }
+
   return (
-    <InternflateDecorator
+    <Decorator
       useProxy
       appName={'Rekrutteringsbistand'}
       environment={miljo}
@@ -28,7 +45,7 @@ const Modiadekoratør: FunctionComponent = () => {
       showSearchArea={false}
       fetchActiveEnhetOnMount={true}
       urlFormat={'NAV_NO'}
-      onEnhetChanged={function (enhetId?: string | null, enhet?: Enhet): void {
+      onEnhetChanged={(_: string | null, enhet?: Enhet): void => {
         setValgtNavKontor({
           navKontor: enhet?.enhetId ?? 'Ukjent navkontor ID',
           navKontorNavn: enhet?.navn ?? 'Ukjent navkontor NAVN',
