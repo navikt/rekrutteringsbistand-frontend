565a64ef82988d4589a5ba426304be14e374b17e
Erlend Wiklem <admin@wiklem.no>
2024-12-03 18:41:46 +0100

    add put

--- app/api/kandidat/[...slug]/route.ts
+++ app/api/kandidat/[...slug]/route.ts
@@ -9,3 +9,7 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
   return proxyWithOBO(KandidatAPI, req);
 }
+
+export async function PUT(req: NextRequest) {
+  return proxyWithOBO(KandidatAPI, req);
+}
