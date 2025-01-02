b0bde8e81f421d5ab1db9cdb6e6ad2d5346b8c36
Erlend Wiklem <admin@wiklem.no>
2025-01-02 10:11:54 +0100

    nanoid

--- app/api/pam-ontologi/stillingsTittel/route.ts
+++ app/api/pam-ontologi/stillingsTittel/route.ts
@@ -1,8 +1,9 @@
+import { nanoid } from 'nanoid';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const søkeord = req.nextUrl.searchParams.get('q');
-
+  const id = nanoid();
   const response = await fetch(
     `${process.env.PAM_ONTOLOGI_URL}/rest/typeahead/stilling?stillingstittel=${søkeord}`,
     {
@@ -11,7 +12,7 @@ export async function GET(req: NextRequest) {
       headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'Cache-Control': 'no-cache, no-store',
-        'Nav-CallId': 'rekrutteringsbistand-frontend',
+        'Nav-CallId': id,
       },
     },
   );
