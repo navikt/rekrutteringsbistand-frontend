5992fa8c3433984af2ec52f822b8c9252e1d8423
Erlend Wiklem <admin@wiklem.no>
2025-01-08 11:30:15 +0100

    wip

--- app/stilling/[stillingsId]/rediger/page.tsx
+++ app/stilling/[stillingsId]/rediger/page.tsx
@@ -11,6 +11,12 @@ import RedigerStilling from './RedigerStilling';
 export default function RedigerSide() {
   const { stillingsData } = useStillingsContext();
 
+  const initialValues = mapStillingTilForm(stillingsData);
+  const registerForm = useForm({
+    resolver: zodResolver(StillingsDataFormSchema),
+    defaultValues: initialValues,
+  });
+
   if (
     stillingsData.stillingsinfo?.stillingskategori ===
     Stillingskategori.Formidling
@@ -18,11 +24,6 @@ export default function RedigerSide() {
     return <RedigerFormidling />;
   }
 
-  const initialValues = mapStillingTilForm(stillingsData);
-  const registerForm = useForm({
-    resolver: zodResolver(StillingsDataFormSchema),
-    defaultValues: initialValues,
-  });
   return (
     <FormProvider {...registerForm}>
       <RedigerStilling />
