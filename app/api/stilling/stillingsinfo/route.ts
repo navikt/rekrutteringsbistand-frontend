a8f39593d3236636df1fd9367d726867313d71de
Erlend Wiklem <admin@wiklem.no>
2025-01-16 22:20:44 +0100

    wip

--- app/api/stilling/stillingsinfo/route.ts
+++ app/api/stilling/stillingsinfo/route.ts
@@ -4,5 +4,5 @@ import { NextRequest } from 'next/server';
 import { StillingAPI } from '../../api-routes';
 
 export async function PUT(req: NextRequest) {
-  return proxyWithOBO(StillingAPI, req, `/rekrutteringsbistandstilling`);
+  return proxyWithOBO(StillingAPI, req, `/stillingsinfo`);
 }
