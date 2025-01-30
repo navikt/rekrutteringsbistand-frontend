15f249918cf635974213eb0dc0e9c0dc9d1f828e
Erlend Wiklem <admin@wiklem.no>
2025-01-30 12:41:47 +0100

    wip stillingssøk

--- app/api/stillings-sok/esFiltre/esStatuser.ts
+++ app/api/stillings-sok/esFiltre/esStatuser.ts
@@ -1,124 +1,98 @@
 import { StillingsStatusTyper } from '../../../stillings-sok/components/StillingsSøkFilter/StatusFilter';
 
 export const esStatuser = (statuser: string[]) => {
-  const statusFilter = [];
+  const ingenFiltreValgt = statuser.length === 0;
+  const alleFiltreValgt =
+    statuser.length === Object.keys(StillingsStatusTyper).length;
 
-  if (statuser.includes(StillingsStatusTyper.Publisert)) {
-    statusFilter.push({
-      bool: {
-        should: [
-          {
-            term: {
-              'stilling.status': 'ACTIVE',
-            },
-          },
-          {
-            bool: {
-              must: [
-                {
-                  term: {
-                    'stilling.status': 'STOPPED',
-                  },
-                },
-                {
-                  term: {
-                    'stilling.administration.status': 'DONE',
-                  },
-                },
-                {
-                  exists: {
-                    field: 'stilling.publishedByAdmin',
-                  },
-                },
-                {
-                  range: {
-                    'stilling.published': {
-                      lte: 'now/d',
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      },
-    });
+  console.log('🎺 statuser', statuser);
+  if (ingenFiltreValgt || alleFiltreValgt) {
+    return alleStillinger;
   }
+  const statusSpørringer: any[] = [];
+  if (statuser.includes(StillingsStatusTyper.Publisert))
+    statusSpørringer.push(publisert);
+  if (statuser.includes(StillingsStatusTyper.Stoppet))
+    statusSpørringer.push(stoppet);
+  if (statuser.includes(StillingsStatusTyper.Utløpt))
+    statusSpørringer.push(utløpt);
 
-  if (statuser.includes(StillingsStatusTyper.Utløpt)) {
-    statusFilter.push({
+  return [
+    {
       bool: {
-        must: [
-          {
-            term: {
-              'stilling.status': 'INACTIVE',
-            },
-          },
-          {
-            range: {
-              'stilling.expires': {
-                lt: 'now/d',
-              },
-            },
-          },
-          {
-            term: {
-              'stilling.administration.status': 'DONE',
-            },
-          },
-          {
-            exists: {
-              field: 'stilling.publishedByAdmin',
-            },
-          },
-          {
-            range: {
-              'stilling.published': {
-                lte: 'now/d',
-              },
-            },
-          },
-        ],
+        should: statusSpørringer,
       },
-    });
-  }
+    },
+  ];
+};
 
-  if (statuser.includes(StillingsStatusTyper.Stoppet)) {
-    statusFilter.push({
-      bool: {
-        should: [
-          {
-            bool: {
-              must: [
-                {
-                  term: {
-                    'stilling.status': 'STOPPED',
-                  },
-                },
-                {
-                  term: {
-                    'stilling.administration.status': 'DONE',
-                  },
-                },
-                {
-                  exists: {
-                    field: 'stilling.publishedByAdmin',
-                  },
-                },
-                {
-                  range: {
-                    'stilling.published': {
-                      lte: 'now/d',
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        ],
+const stillingenErEllerHarVærtPublisert = [
+  {
+    term: {
+      'stilling.administration.status': 'DONE',
+    },
+  },
+  {
+    exists: {
+      field: 'stilling.publishedByAdmin',
+    },
+  },
+  {
+    range: {
+      'stilling.published': {
+        lte: 'now/d',
       },
-    });
-  }
+    },
+  },
+];
 
-  return statusFilter;
+export const alleStillinger = [
+  {
+    bool: {
+      must_not: [
+        {
+          term: {
+            'stilling.status': 'REJECTED',
+          },
+        },
+        {
+          term: {
+            'stilling.status': 'DELETED',
+          },
+        },
+      ],
+      must: stillingenErEllerHarVærtPublisert,
+    },
+  },
+];
+
+const publisert = {
+  term: {
+    'stilling.status': 'ACTIVE',
+  },
+};
+
+const stoppet = {
+  bool: {
+    must: [
+      { term: { 'stilling.status': 'STOPPED' } },
+      ...stillingenErEllerHarVærtPublisert,
+    ],
+  },
+};
+
+const utløpt = {
+  bool: {
+    must: [
+      { term: { 'stilling.status': 'INACTIVE' } },
+      {
+        range: {
+          'stilling.expires': {
+            lt: 'now/d',
+          },
+        },
+      },
+      ...stillingenErEllerHarVærtPublisert,
+    ],
+  },
 };
