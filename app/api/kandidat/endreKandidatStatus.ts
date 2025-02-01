3c7db1475f1b0182d6f76285ac5506f0f7079d13
Erlend Wiklem <admin@wiklem.no>
2025-02-01 20:54:03 +0100

    wip

--- app/api/kandidat/endreKandidatStatus.ts
+++ app/api/kandidat/endreKandidatStatus.ts
@@ -8,6 +8,6 @@ export const endreKandidatStatus = (
 ) => {
   putApi(
     `${KandidatAPI.internUrl}/veileder/kandidatlister/${kandidatlisteId}/kandidater/${kandidatnr}/status`,
-    JSON.stringify({ status }),
+    { status },
   );
 };
