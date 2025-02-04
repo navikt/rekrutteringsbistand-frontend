1cd8c6bea2c0a1ef04d9260d227664441b524fbc
Erlend Wiklem <admin@wiklem.no>
2025-02-04 12:04:22 +0100

    wip skjul ugyldig stilling

--- app/stilling/[stillingsId]/StillingssideLayout.tsx
+++ app/stilling/[stillingsId]/StillingssideLayout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Buildings2Icon, PersonIcon } from '@navikt/aksel-icons';
+import { Alert, Heading } from '@navikt/ds-react';
 import Image from 'next/image';
 import * as React from 'react';
 import StillingsIkon from '../../../public/ikoner/se-mine-stillinger.svg';
@@ -18,9 +19,12 @@ interface StillingSideLayoutProps {
 const StillingSideLayout: React.FC<StillingSideLayoutProps> = ({
   children,
 }) => {
-  const { stillingsData, erEier, erFormidling, erSlettet } =
-    useStillingsContext();
+  const { stillingsData, erFormidling, erSlettet } = useStillingsContext();
   const eierNavn = navnEierAvAstilling(stillingsData);
+
+  const ugyldigStilling =
+    stillingsData?.stilling?.medium === 'DIR' &&
+    (stillingsData?.stilling?.employer?.orgnr ?? null) === null;
 
   return (
     <SideLayout
@@ -58,7 +62,20 @@ const StillingSideLayout: React.FC<StillingSideLayoutProps> = ({
         />
       }
     >
-      {children}
+      {ugyldigStilling ? (
+        <Alert variant='error'>
+          <Heading spacing size='small' level='3'>
+            Ugyldig stilling
+          </Heading>
+          <p>
+            Denne stillingen er ikke gyldig da det er en intern stilling som
+            mangler organisasjonsnummer.
+          </p>
+          <p> Stillingen er derfor ikke tilgjengelig for rekruttering.</p>
+        </Alert>
+      ) : (
+        children
+      )}
     </SideLayout>
   );
 };
