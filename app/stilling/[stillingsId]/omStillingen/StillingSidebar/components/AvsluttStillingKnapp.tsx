acdc8eb231eefd4e3c1dc6c7505e58a8aa0db46e
Erlend Wiklem <admin@wiklem.no>
2025-02-24 15:14:04 +0100

    wip

--- app/stilling/[stillingsId]/omStillingen/StillingSidebar/components/AvsluttStillingKnapp.tsx
+++ app/stilling/[stillingsId]/omStillingen/StillingSidebar/components/AvsluttStillingKnapp.tsx
@@ -29,7 +29,7 @@ const AvsluttStillingKnapp: React.FC<AvsluttStillingKnappProps> = ({
           </BodyLong>
           <BodyLong className='mt-4'>
             Dette avpubliserer stillingen og sender melding til kandidatene som
-            er markert som "fått jobben"
+            er markert som fått jobben.
           </BodyLong>
         </Modal.Body>
         <Modal.Footer>
