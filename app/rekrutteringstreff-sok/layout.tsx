e9030fb40e105fb98bb751f578a9709ea2fae3f5
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-03-04 12:33:17 +0100

    Sett inn opprett treff knapp i rekrutteringstreff oversikt

--- app/rekrutteringstreff-sok/layout.tsx
+++ app/rekrutteringstreff-sok/layout.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@navikt/ds-react';
 import Piktogram from '../../public/ikoner/rekrutteringstreff.svg';
 import SVGDarkmode from '../components/SVGDarkmode';
 import SideLayout from '../components/layout/SideLayout';
 import SideTopBanner from '../components/layout/SideTopBanner';
 import { RekrutteringstreffSøkSidebar } from './components/sidebar/RekrutteringstreffSøkSidebar';
+import { PlusIcon } from '@navikt/aksel-icons';
 import * as React from 'react';
 
 interface layoutProps {
@@ -16,10 +18,13 @@ export default async function RekrutteringstreffSøkLayout({
     <SideLayout
       sidepanel={<RekrutteringstreffSøkSidebar />}
       banner={
-        <SideTopBanner
-          tittel='Rekrutteringstreff'
-          ikon={<SVGDarkmode src={Piktogram} alt='Rekrutteringstreff' />}
-        />
+        <div className="flex justify-between items-center">
+          <SideTopBanner
+            tittel='Rekrutteringstreff'
+            ikon={<SVGDarkmode src={Piktogram} alt='Rekrutteringstreff' />}
+          />
+          <Button variant="primary" icon={<PlusIcon />}>Opprett treff</Button>
+        </div>
       }
     >
       {children}
