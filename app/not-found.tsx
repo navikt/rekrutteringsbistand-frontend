7efb2f53a44b049d917129dac49141f988a33a5b
Erlend Wiklem <admin@wiklem.no>
2025-03-10 15:40:39 +0100

    wip

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,13 +2,16 @@
 
 import { useApplikasjonContext } from './providers/ApplikasjonContext';
 import { NextPage } from 'next';
+import { useEffect } from 'react';
 
 const NotFound: NextPage = () => {
   const { tilUmami } = useApplikasjonContext();
 
-  tilUmami('Fant ikke side (404)', {
-    path: window.location.pathname,
-  });
+  useEffect(() => {
+    tilUmami('Fant ikke side (404)', {
+      path: window.location.pathname,
+    });
+  }, [tilUmami]);
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
