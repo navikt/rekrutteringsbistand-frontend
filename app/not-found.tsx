c3d1cefd7dd9c8bea25523de79408e21ca6977e0
Erlend Wiklem <admin@wiklem.no>
2025-03-10 12:35:41 +0100

    init Umami tracking

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,13 @@
+import { useApplikasjonContext } from './providers/ApplikasjonContext';
 import { NextPage } from 'next';
 
 const NotFound: NextPage = () => {
+  const { tilUmami } = useApplikasjonContext();
+
+  tilUmami('Fant ikke side (404)', {
+    path: window.location.pathname,
+  });
+
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>404 - Fant ikke siden!</h1>
