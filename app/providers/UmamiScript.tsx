5b88aa7cc3d48479a8ac9042e92c4cb69fb40942
Erlend Wiklem <admin@wiklem.no>
2025-03-13 15:13:12 +0100

    test

--- app/providers/UmamiScript.tsx
+++ app/providers/UmamiScript.tsx
@@ -4,6 +4,10 @@ import Script from 'next/script';
 
 export default function UmamiAnalytics() {
   console.log('KAKE');
+  console.log(
+    '🎺 process.env.NAIS_CLUSTER_NAME',
+    process.env.NAIS_CLUSTER_NAME,
+  );
   console.log('umami_src', process.env.NEXT_PUBLIC_UMAMI_SRC);
   console.log('umami_url', process.env.NEXT_PUBLIC_UMAMI_URL);
   console.log('umami_id', process.env.NEXT_PUBLIC_UMAMI_ID);
