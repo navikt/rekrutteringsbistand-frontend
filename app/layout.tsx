3683bb7f12d8f348307004bed649f92f2bd4f12e
Erlend Wiklem <admin@wiklem.no>
2025-03-14 09:35:05 +0100

    NEXT_PUBLIC_DECORATOR_SRC

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,7 @@ import { UmamiProvider } from './providers/UmamiContext';
 import type { Metadata } from 'next';
 import Script from 'next/script';
 
-const devBundle =
-  'https://cdn.nav.no/personoversikt/internarbeidsflate-decorator-v3/dev/latest/dist/bundle.js';
-const prodBundle =
-  'https://cdn.nav.no/personoversikt/internarbeidsflate-decorator-v3/prod/latest/dist/bundle.js';
-
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
   title: isLocal ? 'Local - Rekrutteringsbistand' : 'Rekrutteringsbistand',
@@ -22,16 +17,16 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const bundle =
-    process.env.NAIS_CLUSTER_NAME === 'prod-gcp' ? prodBundle : devBundle;
-
   return (
     <html
       lang='no'
       className='h-full'
       data-testmode={process.env.NEXT_PUBLIC_PLAYWRIGHT_TEST_MODE}
     >
-      <Script src={bundle} strategy='afterInteractive' />
+      <Script
+        src={process.env.NEXT_PUBLIC_DECORATOR_SRC}
+        strategy='afterInteractive'
+      />
       <Script
         defer
         id='umami-analytics'
