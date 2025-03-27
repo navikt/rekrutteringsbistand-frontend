97f25b5239ef1467b85094798ee13fed495ab5d3
Erlend Wiklem <admin@wiklem.no>
2025-03-27 14:25:59 +0100

    Ny visningsstatus

--- app/stilling/[stillingsId]/kandidater/components/VelgInternStatus.tsx
+++ app/stilling/[stillingsId]/kandidater/components/VelgInternStatus.tsx
@@ -1,6 +1,15 @@
 import { endreKandidatStatus } from '../../../../api/kandidat/endreKandidatStatus';
 import { InternKandidatstatus } from './KandidatTyper';
-import { Select } from '@navikt/ds-react';
+import {
+  CircleSlashIcon,
+  ClipboardCheckmarkIcon,
+  ClipboardIcon,
+  FileXMarkIcon,
+  PencilIcon,
+  PersonChatIcon,
+  ReceptionIcon,
+} from '@navikt/aksel-icons';
+import { Button, Dropdown, Tag } from '@navikt/ds-react';
 import * as React from 'react';
 
 export interface VelgInternStatusProps {
@@ -27,23 +36,88 @@ const VelgInternStatus: React.FC<VelgInternStatusProps> = ({
     endreKandidatStatus(kandidatlisteId, kandidatnr, status);
   };
 
+  const formatStatus = (status: string): string => {
+    // Split by underscore and convert each part to PascalCase
+    return status
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+  };
+
+  const formIcon = (status: InternKandidatstatus) => {
+    switch (status) {
+      case InternKandidatstatus.VURDERES:
+        return <ClipboardIcon />;
+      case InternKandidatstatus.KONTAKTET:
+        return <PersonChatIcon />;
+      case InternKandidatstatus.AKTUELL:
+        return <ClipboardCheckmarkIcon />;
+      case InternKandidatstatus.TIL_INTERVJU:
+        return <ReceptionIcon />;
+      case InternKandidatstatus.UAKTUELL:
+        return <FileXMarkIcon />;
+      case InternKandidatstatus.UINTERESSERT:
+        return <CircleSlashIcon />;
+      default:
+        return '';
+    }
+  };
+
+  const tagVariant = (status: InternKandidatstatus) => {
+    switch (status) {
+      case InternKandidatstatus.VURDERES:
+        return 'neutral';
+      case InternKandidatstatus.KONTAKTET:
+        return 'neutral';
+      case InternKandidatstatus.AKTUELL:
+        return 'info';
+      case InternKandidatstatus.TIL_INTERVJU:
+        return 'info';
+      case InternKandidatstatus.UAKTUELL:
+        return 'alt1';
+      case InternKandidatstatus.UINTERESSERT:
+        return 'alt1';
+      default:
+        return 'neutral';
+    }
+  };
+
   return (
-    <Select
-      disabled={lukketKandidatliste}
-      label='Velg intern status'
-      value={valgtStatus}
-      hideLabel
-      onChange={(e) => endreStatus(e.target.value as InternKandidatstatus)}
-    >
-      <option value={InternKandidatstatus.VURDERES}>Vurderes</option>
-      <option value={InternKandidatstatus.KONTAKTET}>Kontaktet</option>
-      <option value={InternKandidatstatus.AKTUELL}>Aktuell</option>
-      <option value={InternKandidatstatus.TIL_INTERVJU}>Til intervju</option>
-      <option value={InternKandidatstatus.UAKTUELL}>Ikke aktuell</option>
-      <option value={InternKandidatstatus.UINTERESSERT}>
-        Ikke interessert
-      </option>
-    </Select>
+    <>
+      <Dropdown>
+        <div className='flex justify-center'>
+          <Tag
+            variant={tagVariant(valgtStatus)}
+            className='flex justify-center gap-2
+            '
+          >
+            {formIcon(valgtStatus)} {formatStatus(valgtStatus)}
+          </Tag>
+          <Button
+            disabled={lukketKandidatliste}
+            size='small'
+            icon={<PencilIcon />}
+            variant='tertiary-neutral'
+            as={Dropdown.Toggle}
+          />
+        </div>
+        <Dropdown.Menu>
+          <Dropdown.Menu.GroupedList>
+            {(Object.keys(InternKandidatstatus) as InternKandidatstatus[]).map(
+              (status) => (
+                <Dropdown.Menu.GroupedList.Item
+                  key={status}
+                  onClick={() => endreStatus(status)}
+                >
+                  {formIcon(status)}
+                  {formatStatus(status)}
+                </Dropdown.Menu.GroupedList.Item>
+              ),
+            )}
+          </Dropdown.Menu.GroupedList>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
   );
 };
 
