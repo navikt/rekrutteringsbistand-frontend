ba08a685c57682731b7ae112d1d0f5e75ffd2961
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-04-04 16:02:22 +0200

    Forsøk uten swrlaster

--- app/rekrutteringstreff/[rekrutteringstreffId]/components/jobbsøkere/Jobbsøkere.tsx
+++ app/rekrutteringstreff/[rekrutteringstreffId]/components/jobbsøkere/Jobbsøkere.tsx
@@ -4,7 +4,6 @@ import { useRekrutteringstreffContext } from '../../RekrutteringstreffContext';
 import LeggTilJobbsøkerKnapp from '../LeggTilJobbsøkerKnapp';
 import JobbsøkerKort from './components/JobbsøkerKort';
 import { useJobbsøkere } from '@/app/api/rekrutteringstreff/[...slug]/useJobbsøkere';
-import SWRLaster from '@/app/components/SWRLaster';
 import { BodyShort } from '@navikt/ds-react';
 import * as React from 'react';
 
@@ -16,42 +15,50 @@ const Jobbsøkere = () => {
   }, []);
 
   const { rekrutteringstreffId } = useRekrutteringstreffContext();
-  const jobbsøkerHook = useJobbsøkere(rekrutteringstreffId);
+
+  const {
+    data: jobbsøkere,
+    isLoading,
+    error,
+    refresh,
+  } = useJobbsøkere(rekrutteringstreffId);
+
+  if (isLoading) {
+    return <BodyShort>Laster jobbsøkere...</BodyShort>;
+  }
+
+  if (error) {
+    return <BodyShort>Kunne ikke hente jobbsøkere</BodyShort>;
+  }
 
   return (
-    <SWRLaster hooks={[jobbsøkerHook]}>
-      {(jobbsøkere) => (
-        <div className='p-4 flex flex-col gap-4'>
-          <div className='flex items-center justify-between'>
-            <LeggTilJobbsøkerKnapp
-              onNyJobbsøkerLagtTil={jobbsøkerHook.refresh}
-            />
-          </div>
+    <div className='p-4 flex flex-col gap-4'>
+      <div className='flex items-center justify-between'>
+        <LeggTilJobbsøkerKnapp onNyJobbsøkerLagtTil={refresh} />
+      </div>
 
-          {jobbsøkere.length === 0 ? (
-            <BodyShort>Ingen jobbsøkere lagt til</BodyShort>
-          ) : (
-            <ul>
-              {jobbsøkere.map((j, index) => (
-                <li key={index}>
-                  <JobbsøkerKort
-                    fornavn={j.fornavn}
-                    etternavn={j.etternavn}
-                    fødselsnummer={j.fødselsnummer}
-                    navKontor='Nav Grorud' // TODO: Bytt når backend støtter det
-                    veileder={{
-                      navn: 'Veileder Navn',
-                      navIdent: 'Z123456',
-                      navKontor: 'Nav Grorud',
-                    }}
-                  />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+      {jobbsøkere && jobbsøkere.length === 0 ? (
+        <BodyShort>Ingen jobbsøkere lagt til</BodyShort>
+      ) : (
+        <ul>
+          {jobbsøkere?.map((j, index) => (
+            <li key={index}>
+              <JobbsøkerKort
+                fornavn={j.fornavn}
+                etternavn={j.etternavn}
+                fødselsnummer={j.fødselsnummer}
+                navKontor='Nav Grorud' // TODO: Bytt når backend støtter det
+                veileder={{
+                  navn: 'Veileder Navn',
+                  navIdent: 'Z123456',
+                  navKontor: 'Nav Grorud',
+                }}
+              />
+            </li>
+          ))}
+        </ul>
       )}
-    </SWRLaster>
+    </div>
   );
 };
 
