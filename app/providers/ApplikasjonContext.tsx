9d6aa5f587c28c7e7cb88fb35634b3412fd83ede
Erlend Wiklem <admin@wiklem.no>
2025-04-23 14:03:08 +0200

    Riktig navn i kommentar

--- app/providers/ApplikasjonContext.tsx
+++ app/providers/ApplikasjonContext.tsx
@@ -70,7 +70,7 @@ export const ApplikasjonContextProvider: React.FC<
     valgtNavKontor?.navKontor === '1001' || // Kristiansand
     valgtNavKontor?.navKontor === '1621' || // Ørland
     valgtNavKontor?.navKontor === '0316' || // Gamle Oslo
-    valgtNavKontor?.navKontor === '0521' || // Ørland
+    valgtNavKontor?.navKontor === '0521' || // Nav Øyer
     valgtNavKontor?.navKontor === '5701' || // FALKENBORG
     valgtNavKontor?.navKontor === '1002' || // LINDESNES
     valgtNavKontor?.navKontor === '0334' || // VESTRE AKER
