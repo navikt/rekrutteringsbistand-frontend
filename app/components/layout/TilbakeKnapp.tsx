5dcefb4308d68366c282f54c77226be2d80fffcf
Erlend Wiklem <admin@wiklem.no>
2025-04-23 15:22:03 +0200

    Fiks tilbakeknapp

--- app/components/layout/TilbakeKnapp.tsx
+++ app/components/layout/TilbakeKnapp.tsx
@@ -2,13 +2,48 @@
 
 import { ArrowLeftIcon } from '@navikt/aksel-icons';
 import { Button } from '@navikt/ds-react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import * as React from 'react';
 
 const TilbakeKnapp: React.FC = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
   const tilbake = () => {
-    router.back();
+    if (typeof window !== 'undefined') {
+      if (window.history.length > 1) {
+        try {
+          const previousPageInSameDomain =
+            document.referrer &&
+            new URL(document.referrer).origin === window.location.origin;
+
+          if (previousPageInSameDomain) {
+            router.back();
+            return;
+          }
+        } catch {}
+      }
+
+      const pathSegments = pathname.split('/').filter(Boolean);
+      if (pathSegments.length > 0) {
+        pathSegments.pop();
+        const parentPath =
+          pathSegments.length === 0 ? '/' : `/${pathSegments.join('/')}`;
+        router.push(parentPath);
+      } else {
+        router.push('/');
+      }
+    } else {
+      const pathSegments = pathname.split('/').filter(Boolean);
+      if (pathSegments.length > 0) {
+        pathSegments.pop();
+        const parentPath =
+          pathSegments.length === 0 ? '/' : `/${pathSegments.join('/')}`;
+        router.push(parentPath);
+      } else {
+        router.push('/');
+      }
+    }
   };
 
   return (
