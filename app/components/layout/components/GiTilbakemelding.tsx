0be18d4d048bbe324a8814ea50ec858234f3ac93
Erlend Wiklem <admin@wiklem.no>
2025-05-14 13:08:16 +0200

    Knapp for tilbakemelding

--- app/components/layout/components/GiTilbakemelding.tsx
+++ app/components/layout/components/GiTilbakemelding.tsx
@@ -1,18 +1,23 @@
+import { useSidebar } from '../../../../components/ui/sidebar';
+import { PersonChatIcon } from '@navikt/aksel-icons';
 import { Button, Popover } from '@navikt/ds-react';
 import { useRef, useState } from 'react';
 
 const GiTilbakemelding = () => {
   const buttonRef = useRef<HTMLButtonElement>(null);
   const [openState, setOpenState] = useState(false);
-
+  const { open } = useSidebar();
   return (
     <>
       <Button
         ref={buttonRef}
         onClick={() => setOpenState(!openState)}
         aria-expanded={openState}
+        variant='tertiary-neutral'
+        icon={<PersonChatIcon />}
+        className={open ? 'w-full text-left justify-start' : ''}
       >
-        Gi tilbakemelding
+        {open && 'Gi tilbakemelding'}
       </Button>
 
       <Popover
@@ -21,12 +26,12 @@ const GiTilbakemelding = () => {
         anchorEl={buttonRef.current}
       >
         <Popover.Content>
-          {/* @ts-expect-error ikke typet */}
+          {/* @ts-expect-error  Ikke typet */}
           <skyra-survey
             className='w-full h-full text-inherit'
             slug='arbeids-og-velferdsetaten-nav/oversikt'
           >
-            {/* @ts-expect-error ikke typet */}
+            {/* @ts-expect-error  Ikke typet */}
           </skyra-survey>
         </Popover.Content>
       </Popover>
