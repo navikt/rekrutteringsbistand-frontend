48f37dd0ad64a0df5f26aa23d649c45b58d84d16
Erlend Wiklem <admin@wiklem.no>
2025-05-21 14:39:12 +0200

    StatusTag

--- app/stilling/[stillingsId]/kandidatliste/components/VelgInternStatus.tsx
+++ app/stilling/[stillingsId]/kandidatliste/components/VelgInternStatus.tsx
@@ -1,16 +1,12 @@
 import { endreKandidatStatus } from '../../../../api/kandidat/endreKandidatStatus';
 import { InternKandidatstatus } from '../KandidatTyper';
 import { useKandidatlisteContext } from '../KandidatlisteContext';
-import {
-  CircleSlashIcon,
-  ClipboardCheckmarkIcon,
-  ClipboardIcon,
-  FileXMarkIcon,
-  PencilIcon,
-  PersonChatIcon,
-  ReceptionIcon,
-} from '@navikt/aksel-icons';
-import { Button, Dropdown, Tag } from '@navikt/ds-react';
+import InternStatusTag, {
+  internStatusIcon,
+  internStatusTekst,
+} from './InternStatusTag';
+import { PencilIcon } from '@navikt/aksel-icons';
+import { Button, Dropdown } from '@navikt/ds-react';
 import * as React from 'react';
 
 export interface VelgInternStatusProps {
@@ -18,6 +14,7 @@ export interface VelgInternStatusProps {
   kandidatnr: string;
   lukketKandidatliste: boolean;
 }
+
 const VelgInternStatus: React.FC<VelgInternStatusProps> = ({
   kandidatnr,
   status,
@@ -37,61 +34,11 @@ const VelgInternStatus: React.FC<VelgInternStatusProps> = ({
     reFetchKandidatliste();
   };
 
-  const formatStatus = (status: string): string => {
-    // Split by underscore and convert each part to PascalCase
-    return status
-      .split('_')
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join(' ');
-  };
-
-  const formIcon = (status: InternKandidatstatus) => {
-    switch (status) {
-      case InternKandidatstatus.VURDERES:
-        return <ClipboardIcon />;
-      case InternKandidatstatus.KONTAKTET:
-        return <PersonChatIcon />;
-      case InternKandidatstatus.AKTUELL:
-        return <ClipboardCheckmarkIcon />;
-      case InternKandidatstatus.TIL_INTERVJU:
-        return <ReceptionIcon />;
-      case InternKandidatstatus.UAKTUELL:
-        return <FileXMarkIcon />;
-      case InternKandidatstatus.UINTERESSERT:
-        return <CircleSlashIcon />;
-      default:
-        return '';
-    }
-  };
-
-  const tagVariant = (status: InternKandidatstatus) => {
-    switch (status) {
-      case InternKandidatstatus.VURDERES:
-        return 'neutral';
-      case InternKandidatstatus.KONTAKTET:
-        return 'neutral';
-      case InternKandidatstatus.AKTUELL:
-        return 'success';
-      case InternKandidatstatus.TIL_INTERVJU:
-        return 'info';
-      case InternKandidatstatus.UAKTUELL:
-        return 'neutral';
-      case InternKandidatstatus.UINTERESSERT:
-        return 'error';
-      default:
-        return 'neutral';
-    }
-  };
-
   return (
     <>
       <Dropdown>
         <div className='flex justify-left'>
-          <Tag size='small' variant={tagVariant(valgtStatus)}>
-            <div className='flex gap-1'>
-              {formIcon(valgtStatus)} {formatStatus(valgtStatus)}
-            </div>
-          </Tag>
+          <InternStatusTag status={valgtStatus} />
           <Button
             disabled={lukketKandidatliste}
             size='small'
@@ -108,8 +55,8 @@ const VelgInternStatus: React.FC<VelgInternStatusProps> = ({
                   key={status}
                   onClick={() => endreStatus(status)}
                 >
-                  {formIcon(status)}
-                  {formatStatus(status)}
+                  {internStatusIcon(status)}
+                  {internStatusTekst(status)}
                 </Dropdown.Menu.GroupedList.Item>
               ),
             )}
