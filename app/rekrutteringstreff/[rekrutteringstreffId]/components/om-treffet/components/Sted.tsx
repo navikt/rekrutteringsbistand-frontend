5d7d0e2d50b71d963a39ea7a8eb8bb89684f5058
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-05-27 12:10:40 +0200

    Legg til controller for endre sted for rekrutteirngstereff

--- app/rekrutteringstreff/[rekrutteringstreffId]/components/om-treffet/components/Sted.tsx
+++ app/rekrutteringstreff/[rekrutteringstreffId]/components/om-treffet/components/Sted.tsx
@@ -1,10 +1,25 @@
-/*import RekrutteringstreffDetalj from '../../RekrutteringstreffDetalj';
+import RekrutteringstreffDetalj from '../../RekrutteringstreffDetalj';
+import { oppdaterRekrutteringstreff } from '@/app/api/rekrutteringstreff/oppdater-rekrutteringstreff/oppdaterRerkutteringstreff';
 import { RekrutteringstreffDTO } from '@/app/api/rekrutteringstreff/useRekrutteringstreff';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { LocationPinIcon } from '@navikt/aksel-icons';
-import { BodyShort, Button } from '@navikt/ds-react';
+import { BodyShort, Button, Modal, TextField } from '@navikt/ds-react';
+import { logger } from '@navikt/next-logger';
 import { PlusIcon } from 'lucide-react';
 import * as React from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const stedSchema = z.object({
+  gateadresse: z.string().trim().min(1, 'Gateadresse må fylles ut'),
+  postnummer: z
+    .string()
+    .trim()
+    .min(1, 'Postnummer må fylles ut')
+    .regex(/^\d{4}$/, 'Postnummer må bestå av 4 siffer'),
+});
+
+const formId = 'skjema-endre-sted';
 
 export type StedFormFields = {
   gateadresse: string;
@@ -23,28 +38,121 @@ const Sted: React.FC<StedProps> = ({
   className,
   onUpdated,
 }) => {
-  const methods = useForm<StedFormFields>({
+  const modalRef = React.useRef<HTMLDialogElement>(null);
+
+  const {
+    handleSubmit,
+    /*setError,
+    clearErrors,
+    register,*/
+    control,
+    reset,
+    formState: { /*errors,*/ isSubmitting, isDirty, isValid },
+  } = useForm<StedFormFields>({
+    resolver: zodResolver(stedSchema),
     defaultValues: {
       gateadresse: rekrutteringstreff.gateadresse || '',
       postnummer: rekrutteringstreff.postnummer || '',
     },
     mode: 'onChange',
   });
+
+  const close = () => {
+    modalRef.current?.close();
+    reset({
+      gateadresse: rekrutteringstreff.gateadresse || '',
+      postnummer: rekrutteringstreff.postnummer || '',
+    });
+  };
+
+  const onSubmit = async (data: StedFormFields) => {
+    const { tittel, beskrivelse, fraTid, tilTid } = rekrutteringstreff;
+
+    try {
+      await oppdaterRekrutteringstreff(rekrutteringstreff.id, {
+        tittel,
+        beskrivelse,
+        gateadresse: data.gateadresse,
+        postnummer: data.postnummer,
+        fraTid,
+        tilTid,
+      });
+      onUpdated();
+      close();
+    } catch (error) {
+      logger.error('Error in postApi:', error);
+    }
+  };
+
+  const disableSave = !isDirty || !isValid || isSubmitting;
+
   return (
-    <RekrutteringstreffDetalj
-      tittelIkon={<LocationPinIcon fontSize='1.5rem' />}
-      tittel='Sted'
-      knapp={
-        <Button icon={<PlusIcon />} variant='tertiary' size='small'>
-          Legg til
-        </Button>
-      }
-      className={className}
-    >
-      <BodyShort size='small'>TODO</BodyShort>
-    </RekrutteringstreffDetalj>
+    <>
+      <RekrutteringstreffDetalj
+        tittelIkon={<LocationPinIcon fontSize='1.5rem' />}
+        tittel='Sted'
+        knapp={
+          <Button icon={<PlusIcon />} variant='tertiary' size='small'>
+            Legg til
+          </Button>
+        }
+        className={className}
+      >
+        <BodyShort size='small'>TODO</BodyShort>
+      </RekrutteringstreffDetalj>
+      <Modal
+        ref={modalRef}
+        header={{ heading: 'Endre navn på treffet' }}
+        width={400}
+        onClose={close}
+      >
+        <Modal.Body>
+          <form
+            id={formId}
+            onSubmit={handleSubmit(onSubmit)}
+            className='space-y-4'
+          >
+            <Controller
+              name='gateadresse'
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  label='Gateadresse'
+                  error={fieldState.error?.message}
+                  autoFocus
+                />
+              )}
+            />
+            <Controller
+              name='postnummer'
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  label='Postnummer'
+                  error={fieldState.error?.message}
+                />
+              )}
+            />
+          </form>
+        </Modal.Body>
+        <Modal.Footer className='pt-2'>
+          <Button
+            type='submit'
+            form={formId}
+            loading={isSubmitting}
+            disabled={disableSave}
+          >
+            Lagre
+          </Button>
+          <Button type='button' variant='secondary' onClick={close}>
+            Avbryt
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
 export default Sted;
-*/
