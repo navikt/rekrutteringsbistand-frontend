bb1c06c4cf5f01889d3958fa6c5d9ac3afd04705
Erlend Wiklem <admin@wiklem.no>
2025-06-16 09:53:40 +0200

    Ikke vis lagre standardsøk hvis man viser kandidatnr

--- app/stilling/components/LagreStandardsøk.tsx
+++ app/stilling/components/LagreStandardsøk.tsx
@@ -3,19 +3,23 @@ import { useUseBrukerStandardSøk } from '../../api/stilling/standardsok/useBruk
 import { FloppydiskIcon } from '@navikt/aksel-icons';
 import { Button } from '@navikt/ds-react';
 import { useSearchParams } from 'next/navigation';
+import { useQueryState } from 'nuqs';
 import * as React from 'react';
 
 const LagreStandardsøk: React.FC = () => {
   const brukerStandardSøkData = useUseBrukerStandardSøk();
   const searchParams = useSearchParams();
   const searchString = new URLSearchParams(searchParams.toString()).toString();
-
+  const [visKandidatnr] = useQueryState('visKandidatnr', {
+    defaultValue: '',
+    clearOnDefault: true,
+  });
   const brukerStandardSøk = searchString === brukerStandardSøkData.data?.søk;
 
   return (
     <Button
       className='whitespace-nowrap'
-      disabled={brukerStandardSøk}
+      disabled={brukerStandardSøk || visKandidatnr !== ''}
       variant='tertiary'
       aria-describedby='lagre-standardsok-beskrivelse'
       icon={<FloppydiskIcon />}
