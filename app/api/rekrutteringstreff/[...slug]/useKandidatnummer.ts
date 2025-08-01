03484eac9a8b903f56b56772b7d4dc159f2af1e5
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-08-01 08:55:25 +0200

    Lag useKandidatnummer

--- app/api/rekrutteringstreff/[...slug]/useKandidatnummer.ts
+++ app/api/rekrutteringstreff/[...slug]/useKandidatnummer.ts
@@ -2,42 +2,30 @@
 
 import { RekrutteringstreffAPI } from '../../api-routes';
 import { getAPIwithSchema } from '../../fetcher';
-import { jobbsøkereMock } from './mocks/jobbsøkereMock';
 import useSWR from 'swr';
 import { z } from 'zod';
 
-export const jobbsøkereEndepunkt = (id: string) =>
-  `${RekrutteringstreffAPI.internUrl}/${id}/jobbsoker`;
+export const kandidatnummerEndepunkt = (personTreffId: string) =>
+  `${RekrutteringstreffAPI.internUrl}/jobbsoker/${personTreffId}/kandidatnummer`;
 
-const HendelseSchema = z.object({
-  id: z.string(),
-  tidspunkt: z.string(),
-  hendelsestype: z.string(),
-  opprettetAvAktørType: z.string(),
-  aktørIdentifikasjon: z.string(),
+const KandidatnummerSchema = z.object({
+  kandidatnummer: z.string(),
 });
 
-const JobbsøkerSchema = z.object({
-  personTreffId: z.string(),
-  fødselsnummer: z.string(),
-  kandidatnummer: z.string().nullable(),
-  fornavn: z.string(),
-  etternavn: z.string(),
-  navkontor: z.string(),
-  veilederNavn: z.string(),
-  veilederNavIdent: z.string(),
-  hendelser: z.array(HendelseSchema),
-});
+export type KandidatnummerDTO = z.infer<typeof KandidatnummerSchema>;
 
-const JobbsøkereSchema = z.array(JobbsøkerSchema);
-
-export type JobbsøkereDTO = z.infer<typeof JobbsøkereSchema>;
-export type JobbsøkerDTO = z.infer<typeof JobbsøkerSchema>;
-
-export const useJobbsøkere = (id: string) => {
-  return useSWR(jobbsøkereEndepunkt(id), getAPIwithSchema(JobbsøkereSchema));
+export const useKandidatnummer = (personTreffId: string | null) => {
+  const swrKey = personTreffId ? kandidatnummerEndepunkt(personTreffId) : null;
+  return useSWR(swrKey, getAPIwithSchema(KandidatnummerSchema));
 };
 
-export const jobbsøkereMirage = (server: any) => {
-  return server.get(jobbsøkereEndepunkt('*'), () => jobbsøkereMock);
+export const kandidatnummerMirage = (server: any) => {
+  server.get(
+    `${RekrutteringstreffAPI.internUrl}/jobbsoker/:personTreffId/kandidatnummer`,
+    () => {
+      return {
+        kandidatnummer: 'PAM0123ABCDE',
+      };
+    },
+  );
 };
