36f0975b39bdd27db206010a4d63b92f9072692e
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-08-21 14:21:51 +0200

    Legg inn ny knapp for å opprette rekrutteringstreff (#111)

--- app/rekrutteringstreff/RekrutteringstreffSøkLayout.tsx
+++ app/rekrutteringstreff/RekrutteringstreffSøkLayout.tsx
@@ -2,10 +2,19 @@
 
 import RekrutteringstreffDark from '../../public/ikoner/rekrutteringstreff-dark.svg';
 import Rekrutteringstreff from '../../public/ikoner/rekrutteringstreff.svg';
+import { RekbisError } from '../../util/rekbisError';
+import { UmamiEvent } from '../../util/umamiEvents';
+import {
+  opprettNyttRekrutteringstreff,
+  OpprettNyttRekrutteringstreffDTO,
+} from '../api/rekrutteringstreff/nytt-rekrutteringstreff/opprettNyttRekrutteringstreff';
 import SVGDarkmode from '../components/SVGDarkmode';
 import SideLayout from '../components/layout/SideLayout';
 import SideTopBanner from '../components/layout/SideTopBanner';
+import { useApplikasjonContext } from '../providers/ApplikasjonContext';
+import { useUmami } from '../providers/UmamiContext';
 import { RekrutteringstreffFilter } from './components/RekrutteringstreffFilter';
+import { Button } from '@navikt/ds-react';
 import * as React from 'react';
 
 export interface RekrutteringstreffSøkLayoutProps {
@@ -15,12 +24,43 @@ export interface RekrutteringstreffSøkLayoutProps {
 const RekrutteringstreffSøkLayout: React.FC<
   RekrutteringstreffSøkLayoutProps
 > = ({ children }) => {
+  const { trackAndNavigate } = useUmami();
+  const { valgtNavKontor } = useApplikasjonContext();
+
+  const handleOpprettRekrutteringstreff = () => {
+    const nyTreff: OpprettNyttRekrutteringstreffDTO = {
+      opprettetAvNavkontorEnhetId: valgtNavKontor?.navKontor || null,
+    };
+
+    opprettNyttRekrutteringstreff(nyTreff)
+      .then((response) => {
+        const id = response.id;
+        trackAndNavigate(
+          UmamiEvent.Sidebar.opprettet_rekrutteringstreff,
+          `/rekrutteringstreff/${id}`,
+        );
+      })
+      .catch((error) => {
+        throw new RekbisError({
+          message: 'Feil ved opprettelse av nytt rekrutteringstreff:',
+          error,
+        });
+      });
+  };
+
   return (
     <SideLayout
       banner={
         <div className='flex justify-between items-center'>
           <SideTopBanner
             tittel='Rekrutteringstreff'
+            knappIBanner={
+              <div>
+                <Button onClick={handleOpprettRekrutteringstreff}>
+                  Nytt rekrutteringstreff
+                </Button>
+              </div>
+            }
             ikon={
               <SVGDarkmode
                 light={Rekrutteringstreff}
