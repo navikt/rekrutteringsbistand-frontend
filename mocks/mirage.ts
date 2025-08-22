474c323310115635428214f9dd55e983fd90a8eb
Erlend Wiklem <admin@wiklem.no>
2025-08-22 15:19:32 +0200

    wip

--- mocks/mirage.ts
+++ mocks/mirage.ts
@@ -129,7 +129,7 @@ export function makeServer({ environment = 'test' } = {}) {
       mockModiaContext(this);
       kandidagsammendragMirage(this);
       // stillingssøk mock kan disables ved ES søk
-      // stillingssøkMirage(this);
+      stillingssøkMirage(this);
       this.passthrough('*');
     },
   });
