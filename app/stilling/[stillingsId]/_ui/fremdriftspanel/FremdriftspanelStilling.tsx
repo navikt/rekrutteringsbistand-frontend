df2b2bb8bb569de49eba39bece8f7dc1c6f0a84b
Erlend Wiklem <admin@wiklem.no>
2025-09-10 10:38:42 +0200

    Fullført

--- app/stilling/[stillingsId]/_ui/fremdriftspanel/FremdriftspanelStilling.tsx
+++ app/stilling/[stillingsId]/_ui/fremdriftspanel/FremdriftspanelStilling.tsx
@@ -4,9 +4,11 @@ import { useStillingsContext } from '@/app/stilling/[stillingsId]/StillingsConte
 import EndreSøkeforslag from '@/app/stilling/[stillingsId]/_ui/fremdriftspanel/EndreSøkeforslag';
 import RedigerStillingKnapp from '@/app/stilling/[stillingsId]/_ui/fremdriftspanel/RedigerStillingKnapp';
 import FullførStillingKnapp from '@/app/stilling/[stillingsId]/_ui/fremdriftspanel/fullfør-stilling/FullførStillingKnapp';
+import GjenåpneStillingKnapp from '@/app/stilling/[stillingsId]/_ui/fremdriftspanel/fullfør-stilling/GjenåpneStillingKnapp';
 import { StillingsStatus } from '@/app/stilling/_ui/stilling-typer';
 import { TilgangskontrollForInnhold } from '@/components/tilgangskontroll/TilgangskontrollForInnhold';
 import { Roller } from '@/components/tilgangskontroll/roller';
+import { formaterNorskDato } from '@/util/util';
 import {
   BellIcon,
   EyeIcon,
@@ -39,8 +41,62 @@ export default function FremdriftspanelStilling({
         Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
       ]}
     >
-      {erFullført ? (
-        <> Fullført stilling </>
+      {!erFullført ? (
+        <div className={dropDown ? 'p-4' : ''}>
+          <GjenåpneStillingKnapp />
+          <div className='flex flex-col gap-6 mt-6'>
+            <div>
+              <Heading size='small' level='2'>
+                Oppdrag fullført
+              </Heading>
+              <BodyShort
+                size='small'
+                className='text-[var(--ax-text-neutral-subtle)] mt-1'
+              >
+                Av {stillingsData.stilling?.administration?.reportee}{' '}
+                {formaterNorskDato({ dato: stillingsData.stilling.updated })}
+              </BodyShort>
+            </div>
+            <Box.New
+              background='neutral-soft'
+              borderRadius={'large'}
+              padding='3'
+            >
+              <Heading size='xsmall' level='3' className='mb-4'>
+                Hva som skjedde bak kulissene
+              </Heading>
+              <div className='flex gap-4 flex-col'>
+                <div className='flex gap-2'>
+                  <BellIcon aria-hidden className='shrink-0' />
+                  <BodyShort size='small'>
+                    Alle som ikke fikk jobben fikk beskjed om avslaget på
+                    nav.no/min-side.
+                  </BodyShort>
+                </div>
+                <div className='flex gap-2'>
+                  <TableIcon aria-hidden className='shrink-0' />
+                  <BodyShort size='small'>
+                    Aktivitetskortet ble flyttet til “Fullført”-kolonnen i
+                    aktivitetsplanen.
+                  </BodyShort>
+                </div>
+                <div className='flex gap-2'>
+                  <PersonChatIcon aria-hidden className='shrink-0' />
+                  <BodyShort size='small'>
+                    De som fikk jobben fikk ikke beskjed automatisk, siden de
+                    mest sannsynlig får høre nyheten direkte fra arbeidsgiveren.
+                  </BodyShort>
+                </div>
+                <div className='flex gap-2'>
+                  <EyeIcon aria-hidden className='shrink-0' />
+                  <BodyShort size='small'>
+                    Annonsen vises ikke lenger som aktiv.
+                  </BodyShort>
+                </div>
+              </div>
+            </Box.New>
+          </div>
+        </div>
       ) : (
         <div className={dropDown ? 'p-4' : ''}>
           <div className='grid grid-cols-2 gap-2'>
---
+++ app/stilling/[stillingsId]/_ui/fremdriftspanel/fullfør-stilling/GjenåpneStillingKnapp.tsx
@@ -0,0 +1,66 @@
+import { Kandidatlistestatus } from '@/app/api/kandidat/schema.zod';
+import { setKandidatlisteStatus } from '@/app/api/kandidat/setKandidatlisteStatus';
+import { useKandidatlisteForEier } from '@/app/api/kandidat/useKandidatlisteForEier';
+import { oppdaterStilling } from '@/app/api/stilling/oppdater-stilling/oppdaterStilling';
+import { useStillingsContext } from '@/app/stilling/[stillingsId]/StillingsContext';
+import { StillingsStatus } from '@/app/stilling/_ui/stilling-typer';
+import SWRLaster from '@/components/SWRLaster';
+import { useApplikasjonContext } from '@/providers/ApplikasjonContext';
+import { RekbisError } from '@/util/rekbisError';
+import { Button } from '@navikt/ds-react';
+import { useState } from 'react';
+
+export default function GjenåpneStillingKnapp() {
+  const { stillingsData, refetch, erEier } = useStillingsContext();
+  const { valgtNavKontor, brukerData, visVarsel } = useApplikasjonContext();
+  const kandidatlisteForEier = useKandidatlisteForEier(stillingsData, erEier);
+  const [loading, setLoading] = useState(false);
+  const gjenåpne = async (kandidatlisteId: string) => {
+    setLoading(true);
+    try {
+      await Promise.all([
+        setKandidatlisteStatus(kandidatlisteId, Kandidatlistestatus.Lukket),
+        oppdaterStilling(
+          {
+            ...stillingsData,
+            stilling: {
+              ...stillingsData.stilling,
+              status: StillingsStatus.Stoppet,
+            },
+          },
+          {
+            eierNavident: brukerData.ident,
+            eierNavn: brukerData.navn,
+            eierNavKontorEnhetId: valgtNavKontor?.navKontor,
+          },
+        ),
+      ]);
+      visVarsel({ type: 'success', tekst: 'Du har nå fullført oppdraget.' });
+      refetch?.();
+    } catch (error) {
+      visVarsel({
+        type: 'error',
+        tekst: 'Klarte ikke å endre status på oppdraget',
+      });
+      new RekbisError({ message: 'Klarte ikke å fullføre oppdrag', error });
+    }
+    setLoading(false);
+  };
+
+  return (
+    <SWRLaster hooks={[kandidatlisteForEier]}>
+      {(kandidatlisteForEier) => {
+        return (
+          <Button
+            loading={loading}
+            size='small'
+            className='w-full'
+            onClick={() => gjenåpne(kandidatlisteForEier.kandidatlisteId)}
+          >
+            Gjenåpne
+          </Button>
+        );
+      }}
+    </SWRLaster>
+  );
+}
