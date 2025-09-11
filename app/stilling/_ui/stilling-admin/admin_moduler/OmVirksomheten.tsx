67503be83d5b81f5c64da1396c57e20f64f35447
haugse <sara.haugse@gmail.com>
2025-09-11 10:31:42 +0200

    Sett businessName når employer velges
    Co-authored-by: Ken Ove Andersen <ken.ove.andersen@nav.no>

--- app/stilling/_ui/stilling-admin/admin_moduler/OmVirksomheten.tsx
+++ app/stilling/_ui/stilling-admin/admin_moduler/OmVirksomheten.tsx
@@ -106,6 +106,7 @@ export default function OmVirksomheten() {
   const setArbeidsgiver = (arbeidsgiver: ArbeidsgiverDTO) => {
     setArbeidsgiverState(arbeidsgiver);
     const eksisterende = getValues('stilling.employer') ?? ({} as any);
+    setValue('stilling.businessName', arbeidsgiver.navn ?? null);
     setValue('stilling.employer', {
       ...eksisterende,
       orgnr: arbeidsgiver.organisasjonsnummer ?? '',
