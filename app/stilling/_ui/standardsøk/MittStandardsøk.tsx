73ece097a155233d8ae86e391dc1c2c85bcd245a
Erlend Wiklem <admin@wiklem.no>
2025-09-11 19:08:15 +0200

    standardsøk

--- app/stilling/_ui/standardsøk/MittStandardsøk.tsx
+++ app/stilling/_ui/standardsøk/MittStandardsøk.tsx
@@ -1,18 +1,26 @@
 import { useUseBrukerStandardSøk } from '@/app/api/stilling/standardsok/useBrukersStandardsøk';
 import { byggStandardsokQuery } from '@/app/stilling/_ui/standardsøk/standardSokUtils';
 import { Switch } from '@navikt/ds-react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function MittStandardsøk() {
   const brukerStandardSøkData = useUseBrukerStandardSøk();
   const searchParams = useSearchParams();
-
+  const router = useRouter();
   const { query: searchString } = byggStandardsokQuery(searchParams);
   const brukerStandardSøk = searchString === brukerStandardSøkData.data?.søk;
 
   return (
     <div>
-      <Switch checked={brukerStandardSøk} disabled size='small'>
+      <Switch
+        checked={brukerStandardSøk}
+        size='small'
+        onChange={(e) => {
+          if (!brukerStandardSøk) {
+            router.push('/stilling?brukStandardsok=true');
+          }
+        }}
+      >
         Mitt standardsøk
       </Switch>
     </div>
