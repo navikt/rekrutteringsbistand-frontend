13e03bb2af65f599640d538cd1ed86c569c8e155
Erlend Wiklem <admin@wiklem.no>
2025-09-17 12:15:42 +0200

    aksel farge

--- components/layout/sidebar/UlesteNyheterWrapper.tsx
+++ components/layout/sidebar/UlesteNyheterWrapper.tsx
@@ -39,7 +39,7 @@ export default function UlesteNyheterWrapper({
   return (
     <>
       {harUlestNyhet && (
-        <div className='absolute top-2 left-9 w-3 h-3 bg-red-500 rounded-full'></div>
+        <div className='absolute top-2 left-9 w-3 h-3 bg-[var(--ax-bg-danger-strong)] rounded-full'></div>
       )}
       {children}
     </>
