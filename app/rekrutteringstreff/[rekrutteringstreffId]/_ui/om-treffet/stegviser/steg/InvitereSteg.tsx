c7a20ba946a5c5051a1d4e602d6cad1c1fc05e04
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-09-17 13:08:46 +0200

    Legg inn fra nytt design for invitasjonssteget i rekrutteringstreff

--- app/rekrutteringstreff/[rekrutteringstreffId]/_ui/om-treffet/stegviser/steg/InvitereSteg.tsx
+++ app/rekrutteringstreff/[rekrutteringstreffId]/_ui/om-treffet/stegviser/steg/InvitereSteg.tsx
@@ -1,24 +1,91 @@
 'use client';
 
-import { SjekklisteContainer, SjekklisteRad } from './Sjekkliste';
+import {
+  SjekklisteContainer,
+  SjekklisteRad,
+  SjekklisteInfo,
+} from './Sjekkliste';
 import { useStegviser } from '@/app/rekrutteringstreff/[rekrutteringstreffId]/_ui/om-treffet/stegviser/StegviserContext';
-import { BodyShort } from '@navikt/ds-react';
+import {
+  ClockDashedIcon,
+  EyeSlashIcon,
+  TasklistIcon,
+} from '@navikt/aksel-icons';
+import { BodyShort, Detail, HStack, Heading, VStack } from '@navikt/ds-react';
 import * as React from 'react';
 
 const InvitereSteg = () => {
-  const { harInvitert, arrangementtidspunktHarPassert, antallInviterte } =
-    useStegviser();
+  const {
+    harInvitert,
+    tiltidspunktHarPassert,
+    antallInviterte,
+    antallSvarJa,
+    antallVenterSvar,
+  } = useStegviser();
 
   return (
     <SjekklisteContainer>
-      <SjekklisteRad erOppfylt={harInvitert} label='Minst en invitasjon' />
+      <Detail spacing>
+        Noen detaljer være på plass før treffet kan fullføres.
+      </Detail>
+
+      {/* Tellelinjer */}
+      <div className='flex items-center justify-between my-2 text-sm'>
+        <span>Inviterte som har svart ja</span>
+        <span className='tabular-nums'>{antallSvarJa}</span>
+      </div>
+      <div className='flex items-center justify-between my-2 text-sm'>
+        <span>Du venter fortsatt på svar fra</span>
+        <span className='tabular-nums'>{antallVenterSvar}</span>
+      </div>
+
+      {/* Sjekkpunkter */}
       <SjekklisteRad
-        erOppfylt={arrangementtidspunktHarPassert}
-        label='Arrangementets fradato har passert'
+        erOppfylt={harInvitert}
+        label='Minst 1 invitert jobbsøker'
+      />
+      <SjekklisteRad
+        erOppfylt={tiltidspunktHarPassert}
+        label='Tidspunktet for treffet er passert'
       />
       <BodyShort size='small'>
-        Antall inviterte: <b>{antallInviterte}</b>
+        Totalt inviterte: <b>{antallInviterte}</b>
       </BodyShort>
+
+      <SjekklisteInfo>
+        <VStack gap='2'>
+          <Heading level='3' size='small'>
+            Hva skjer når du fullfører?
+          </Heading>
+          <VStack gap='2'>
+            <HStack gap='2' align='start'>
+              <div className='flex-none w-6 mt-[2px]'>
+                <ClockDashedIcon fontSize='1.5rem' aria-hidden />
+              </div>
+              <BodyShort className='flex-1'>
+                Du kan gjenåpne treffet for å rette feil i ettertid.
+              </BodyShort>
+            </HStack>
+            <HStack gap='2' align='start'>
+              <div className='flex-none w-6 mt-[2px]'>
+                <TasklistIcon fontSize='1.5rem' aria-hidden />
+              </div>
+              <BodyShort className='flex-1'>
+                Aktivitetskortet til alle som svarte ja blir flyttet til
+                &quot;Fullført&quot;-kolonnen i aktivitetsplanen.
+              </BodyShort>
+            </HStack>
+            <HStack gap='2' align='start'>
+              <div className='flex-none w-6 mt-[2px]'>
+                <EyeSlashIcon fontSize='1.5rem' aria-hidden />
+              </div>
+              <BodyShort className='flex-1'>
+                Treffet vises ikke lenger som aktivt.
+              </BodyShort>
+            </HStack>
+          </VStack>
+        </VStack>
+      </SjekklisteInfo>
     </SjekklisteContainer>
   );
 };
--- app/rekrutteringstreff/[rekrutteringstreffId]/_ui/om-treffet/stegviser/steg/Sjekkliste.tsx
+++ app/rekrutteringstreff/[rekrutteringstreffId]/_ui/om-treffet/stegviser/steg/Sjekkliste.tsx
@@ -1,5 +1,5 @@
 import { CheckmarkIcon } from '@navikt/aksel-icons';
-import { Box } from '@navikt/ds-react';
+import { Box, Heading } from '@navikt/ds-react';
 import { FC, ReactNode } from 'react';
 
 export const SjekklisteContainer: FC<{ children: ReactNode }> = ({
@@ -7,6 +7,9 @@ export const SjekklisteContainer: FC<{ children: ReactNode }> = ({
 }) => (
   <div className='flex-1'>
     <Box.New padding='0' className='mb-4'>
+      <Heading level='3' size='small' className='mb-2'>
+        Sjekkliste
+      </Heading>
       <div className='space-y-0'>{children}</div>
     </Box.New>
   </div>
