e1536529aec857f12f09c471b7f84065cbcf8eff
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2025-10-01 15:25:37 +0200

    Code reformat

--- app/stilling/[stillingsId]/_ui/StillingsutkastMelding.tsx
+++ app/stilling/[stillingsId]/_ui/StillingsutkastMelding.tsx
@@ -1,11 +1,11 @@
 import { useStillingsContext } from '@/app/stilling/[stillingsId]/StillingsContext';
-import { Button, Heading } from '@navikt/ds-react';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import UtkastFigur from '@/public/illustrasjoner/figur-med-verktøy.svg';
 import EndreStillingStatus from '@/app/stilling/_ui/stilling-admin/admin_moduler/_felles/EndreStillingStatus';
 import { StillingsStatus } from '@/app/stilling/_ui/stilling-typer';
+import UtkastFigur from '@/public/illustrasjoner/figur-med-verktøy.svg';
 import { TrashIcon } from '@navikt/aksel-icons';
+import { Button, Heading } from '@navikt/ds-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function StillingsutkastMelding() {
   const { stillingsData, erEier, kandidatlisteInfo } = useStillingsContext();
@@ -23,8 +23,10 @@ export default function StillingsutkastMelding() {
       <Heading level='5' size='small'>
         Oppdraget er ikke publisert enda
       </Heading>
-      <p className={'pb-10'}>Fortsett der du slapp, eller slett det hvis du vil</p>
-      <div className={'flex gap-2 pb-5 '}>
+      <p className={'pb-10'}>
+        Fortsett der du slapp, eller slett det hvis du vil
+      </p>
+      <div className={'flex gap-2 pb-5'}>
         <Button
           variant={'primary'}
           className={'w-72 h-12'}
