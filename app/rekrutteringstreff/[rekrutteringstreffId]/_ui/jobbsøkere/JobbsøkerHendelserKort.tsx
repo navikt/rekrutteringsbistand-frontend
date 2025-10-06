d4c72a012d0ff7d65da56f435b5e74ec52c509cb
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-10-06 09:28:30 +0200

    Legg til fiks for fullfør knappen og en estetisk fiks

--- app/rekrutteringstreff/[rekrutteringstreffId]/_ui/jobbsøkere/JobbsøkerHendelserKort.tsx
+++ app/rekrutteringstreff/[rekrutteringstreffId]/_ui/jobbsøkere/JobbsøkerHendelserKort.tsx
@@ -44,121 +44,105 @@ const JobbsøkerHendelserKort: FC<JobbsøkerHendelserKortProps> = ({
   const siste5Hendelser = jobbsøkerHendelserDTO.slice(-5);
 
   return (
-    <div>
-      <Box.New
-        background='neutral-softA'
-        className='mb-4 flex flex-col h-full'
-        borderColor='neutral-subtleA'
-        borderRadius='xlarge'
-        borderWidth='1'
-        padding='6'
-      >
-        <div>
-          <Heading level='2' size='small' className='mb-4 text-left'>
-            Jobbsøkere
-          </Heading>
-          <div className='min-h-[18rem]'>
-            {antallHendelser === 0 ? (
-              <div className='p-4 flex flex-col items-center'>
-                <Box.New
-                  background='neutral-softA'
-                  className='rounded-full mb-2'
-                >
-                  <SVGDarkmode
-                    light={JobbsokerHeartUpIkon}
-                    dark={JobbsokerHeartUpDarkIkon}
-                    alt='legg_til_jobbsøker'
-                  />
-                </Box.New>
-                <BodyShort className='text-center'>
-                  <span className='block'>
-                    Finn og legg til en jobbsøker så dukker aktivitetene deres
-                    opp her.
-                  </span>
-                </BodyShort>
-              </div>
-            ) : (
-              <div>
-                <div className='flex flex-wrap gap-2'>
-                  <JobbsøkerHendelseLabel
-                    icon={<PlusCircleIcon className='text-white' />}
-                    hendelseType={JobbsøkerHendelsestype.OPPRETT}
-                    antall={antallLagtTilHendelser}
-                  />
-                  <JobbsøkerHendelseLabel
-                    icon={<EnvelopeClosedIcon className='text-blue-400' />}
-                    hendelseType={JobbsøkerHendelsestype.INVITER}
-                    antall={antallInviterte}
-                  />
-                  <JobbsøkerHendelseLabel
-                    icon={<CheckmarkCircleIcon className='text-green-500' />}
-                    hendelseType={JobbsøkerHendelsestype.SVAR_JA_TIL_INVITASJON}
-                    antall={antallSvarJa}
-                  />
-                  <JobbsøkerHendelseLabel
-                    icon={<XMarkOctagonIcon className='text-violet-300' />}
-                    hendelseType={
-                      JobbsøkerHendelsestype.SVAR_NEI_TIL_INVITASJON
-                    }
-                    antall={antallSvarNei}
-                  />
-                  <JobbsøkerHendelseLabel
-                    icon={<QuestionmarkDiamondIcon className='text-sky-300' />}
-                    hendelseType={'ubesvart'}
-                    antall={antallUbesvart}
-                  />
-                </div>
+    <Box.New
+      background='neutral-softA'
+      className='mb-4 flex flex-col h-full'
+      borderColor='neutral-subtleA'
+      borderRadius='xlarge'
+      borderWidth='1'
+      padding='6'
+    >
+      <Heading level='2' size='small' className='mb-4 text-left'>
+        Jobbsøkere
+      </Heading>
+      <div className='min-h-[18rem] flex-1'>
+        {antallHendelser === 0 ? (
+          <div className='p-4 flex flex-col items-center'>
+            <Box.New background='neutral-softA' className='rounded-full mb-2'>
+              <SVGDarkmode
+                light={JobbsokerHeartUpIkon}
+                dark={JobbsokerHeartUpDarkIkon}
+                alt='legg_til_jobbsøker'
+              />
+            </Box.New>
+            <BodyShort className='text-center'>
+              <span className='block'>
+                Finn og legg til en jobbsøker så dukker aktivitetene deres opp
+                her.
+              </span>
+            </BodyShort>
+          </div>
+        ) : (
+          <div className='mb-12'>
+            <div className='flex flex-wrap gap-2'>
+              <JobbsøkerHendelseLabel
+                icon={<PlusCircleIcon className='text-white' />}
+                hendelseType={JobbsøkerHendelsestype.OPPRETT}
+                antall={antallLagtTilHendelser}
+              />
+              <JobbsøkerHendelseLabel
+                icon={<EnvelopeClosedIcon className='text-blue-400' />}
+                hendelseType={JobbsøkerHendelsestype.INVITER}
+                antall={antallInviterte}
+              />
+              <JobbsøkerHendelseLabel
+                icon={<CheckmarkCircleIcon className='text-green-500' />}
+                hendelseType={JobbsøkerHendelsestype.SVAR_JA_TIL_INVITASJON}
+                antall={antallSvarJa}
+              />
+              <JobbsøkerHendelseLabel
+                icon={<XMarkOctagonIcon className='text-violet-300' />}
+                hendelseType={JobbsøkerHendelsestype.SVAR_NEI_TIL_INVITASJON}
+                antall={antallSvarNei}
+              />
+              <JobbsøkerHendelseLabel
+                icon={<QuestionmarkDiamondIcon className='text-sky-300' />}
+                hendelseType={'ubesvart'}
+                antall={antallUbesvart}
+              />
+            </div>
 
-                <Heading size='xsmall' level='4' className='mt-8 mb-2'>
-                  Siste hendelser
-                </Heading>
+            <Heading size='xsmall' level='4' className='mt-8 mb-2'>
+              Siste hendelser
+            </Heading>
 
-                <div className='mt-4'>
-                  {siste5Hendelser.map((hendelse) => (
-                    <div
+            <div className='mt-4'>
+              {siste5Hendelser.map((hendelse) => (
+                <div key={hendelse.id} className='flex flex-wrap gap-2 mb-4'>
+                  <div className='mb-2  min-w-[10rem]'>
+                    <JobbsøkerHendelseLabel
                       key={hendelse.id}
-                      className='flex flex-wrap gap-2 mb-4'
-                    >
-                      <div className='mb-2  min-w-[10rem]'>
-                        <JobbsøkerHendelseLabel
-                          key={hendelse.id}
-                          icon={<PlusCircleIcon className='text-white' />}
-                          hendelseType={hendelse.hendelsestype}
+                      icon={<PlusCircleIcon className='text-white' />}
+                      hendelseType={hendelse.hendelsestype}
+                    />
+                    <BodyShort className='ml-6'>
+                      {format(new Date(hendelse.tidspunkt), 'dd. MMMM yyyy', {
+                        locale: nb,
+                      })}
+                    </BodyShort>
+                  </div>
+                  <div>
+                    {hendelse.fornavn &&
+                      hendelse.etternavn &&
+                      hendelse.kandidatnummer && (
+                        <NavnLenke
+                          fornavn={hendelse.fornavn}
+                          etternavn={hendelse.etternavn}
+                          personTreffId={hendelse.personTreffId}
                         />
-                        <BodyShort className='ml-6'>
-                          {format(
-                            new Date(hendelse.tidspunkt),
-                            'dd. MMMM yyyy',
-                            { locale: nb },
-                          )}
-                        </BodyShort>
-                      </div>
-                      <div>
-                        {hendelse.fornavn &&
-                          hendelse.etternavn &&
-                          hendelse.kandidatnummer && (
-                            <NavnLenke
-                              fornavn={hendelse.fornavn}
-                              etternavn={hendelse.etternavn}
-                              personTreffId={hendelse.personTreffId}
-                            />
-                          )}
-                        {hendelse.fødselsnummer && (
-                          <BodyShort>{hendelse.fødselsnummer}</BodyShort>
-                        )}
-                      </div>
-                    </div>
-                  ))}
+                      )}
+                    {hendelse.fødselsnummer && (
+                      <BodyShort>{hendelse.fødselsnummer}</BodyShort>
+                    )}
+                  </div>
                 </div>
-              </div>
-            )}
+              ))}
+            </div>
           </div>
-          <div className='mt-auto'>
-            <LeggTilJobbsøkerKnapp className='w-full max-w-2xl' />
-          </div>
-        </div>
-      </Box.New>
-    </div>
+        )}
+      </div>
+      <LeggTilJobbsøkerKnapp className='w-full mt-auto' />
+    </Box.New>
   );
 };
 
