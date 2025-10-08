a338f90dddcf79fcfc4573ea11c86069d39f9a9f
Erlend Wiklem <admin@wiklem.no>
2025-10-08 13:36:51 +0200

    wops

--- app/api/stilling/rekrutteringsbistandstilling/[slug]/slett-stilling.ts
+++ app/api/stilling/rekrutteringsbistandstilling/[slug]/slett-stilling.ts
@@ -1,7 +1,8 @@
+import { StillingAPI } from '@/app/api/api-routes';
 import { deleteApi } from '@/app/api/fetcher';
 
 const slettStillingEndepunkt = (stillingsId: string) =>
-  `/stilling/rekrutteringsbistandstilling/${stillingsId}`;
+  `${StillingAPI.internUrl}/rekrutteringsbistandstilling/${stillingsId}`;
 
 export async function slettStilling(stillingsId: string) {
   return await deleteApi(slettStillingEndepunkt(stillingsId));
