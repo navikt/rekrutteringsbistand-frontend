8342ece7a0e0754a82e1f3ec4c5ce2678a5d1c60
Erlend Wiklem <admin@wiklem.no>
2025-10-27 11:30:09 +0100

    Padding fix

--- app/stilling/[stillingsId]/_ui/om-stillingen/OmStillingenHeader.tsx
+++ app/stilling/[stillingsId]/_ui/om-stillingen/OmStillingenHeader.tsx
@@ -5,7 +5,7 @@ import { Heading } from '@navikt/ds-react';
 export default function OmStillingenHeader() {
   const { stillingsData } = useStillingsContext();
   return (
-    <div className='py-5'>
+    <div className='pb-5'>
       <Heading size='large'>{stillingsData.stilling.title ?? ''}</Heading>{' '}
       <StillingsTag stillingsData={stillingsData} rad />
     </div>
