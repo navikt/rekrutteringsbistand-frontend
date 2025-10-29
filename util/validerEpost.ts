7833e465f10089652ab588d97d2c3adce4ebfb68
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2025-10-29 13:10:22 +0100

    Test validerEpost opp mot trimmet e-post

--- util/validerEpost.ts
+++ util/validerEpost.ts
@@ -2,9 +2,10 @@ export const validerEpost = (
   epost: string,
 ): { erGodkjent: boolean; feilmelding: string } => {
   const emailRegex = /^[\p{L}\p{N}._%+-]+@[\p{L}\p{N}.-]+\.\p{L}{2,}$/u;
-  return epost.trim() === ''
+  const trimmetEpost = epost.trim();
+  return trimmetEpost === ''
     ? { erGodkjent: false, feilmelding: 'E-post kan ikke være tom' }
-    : emailRegex.test(epost)
+    : emailRegex.test(trimmetEpost)
       ? { erGodkjent: true, feilmelding: '' }
       : { erGodkjent: false, feilmelding: 'Ugyldig e-postadresse' };
 };
