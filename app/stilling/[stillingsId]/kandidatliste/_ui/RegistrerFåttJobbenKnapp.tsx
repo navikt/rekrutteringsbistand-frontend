717170501b0f9656b89cc0bfc1ba1c44c317f237
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2025-11-17 11:47:03 +0100

    Legg til mutate og refetch av kandidater som har fått jobb

--- app/stilling/[stillingsId]/kandidatliste/_ui/RegistrerFåttJobbenKnapp.tsx
+++ app/stilling/[stillingsId]/kandidatliste/_ui/RegistrerFåttJobbenKnapp.tsx
@@ -1,11 +1,10 @@
+import { useKandidatlisteForEier } from '@/app/api/kandidat/useKandidatlisteForEier';
+import { useNullableStillingsContext } from '@/app/stilling/[stillingsId]/StillingsContext';
+import FullførStillingModal from '@/app/stilling/[stillingsId]/_ui/fremdriftspanel/fullfør-stilling/FullførStillingModal';
 import { KandidatutfallTyper } from '@/app/stilling/[stillingsId]/kandidatliste/KandidatTyper';
 import { ClipboardCheckmarkIcon } from '@navikt/aksel-icons';
 import { ActionMenu, Button } from '@navikt/ds-react';
 import { FC, useState } from 'react';
-import { useKandidatlisteForEier } from '@/app/api/kandidat/useKandidatlisteForEier';
-import { useStillingsContext } from '@/app/stilling/[stillingsId]/StillingsContext';
-import FullførStillingModal
-  from '@/app/stilling/[stillingsId]/_ui/fremdriftspanel/fullfør-stilling/FullførStillingModal';
 
 
 export interface RegistrerFåttJobbenKnappProps {
@@ -22,26 +21,33 @@ const RegistrerFåttJobbenKnapp: FC<RegistrerFåttJobbenKnappProps> = ({
   actionMenu,
 }) => {
   const [visFullførStillingModal, setVisFullførStillingModal] = useState(false);
-  const { stillingsData, erEier } = useStillingsContext();
-  const kandidatlisteForEier = useKandidatlisteForEier(stillingsData, erEier).data;
+  const stillingsContext = useNullableStillingsContext();
+  const kandidatlisteForEier = useKandidatlisteForEier(
+    stillingsContext?.stillingsData,
+    stillingsContext?.erEier,
+  );
 
-  const sjekkOmAlleStillingerBesatt = () => {
+  const håndterKnappetrykk = async () => {
+    endreUtfallForKandidat(KandidatutfallTyper.FATT_JOBBEN);
+
+    await kandidatlisteForEier.mutate();
+    stillingsContext?.refetchKandidatliste?.();
+
     const ikkeArkiverteKandidater =
-      kandidatlisteForEier?.kandidater?.filter((k) => !k.arkivert) ?? [];
+      kandidatlisteForEier.data?.kandidater?.filter((k) => !k.arkivert) ?? [];
 
     const antallKandidaterSomHarFåttJobb =
-      ikkeArkiverteKandidater.filter(
-        (k) => k.utfall === KandidatutfallTyper.FATT_JOBBEN,
-      ).length +
-      (kandidatlisteForEier?.formidlingerAvUsynligKandidat?.filter(
-        (k) => k.utfall === KandidatutfallTyper.FATT_JOBBEN,
-      )?.length || 0);
+    ikkeArkiverteKandidater.filter(
+      (k) => k.utfall === KandidatutfallTyper.FATT_JOBBEN,
+    ).length +
+    (kandidatlisteForEier.data?.formidlingerAvUsynligKandidat?.filter(
+      (k) => k.utfall === KandidatutfallTyper.FATT_JOBBEN,
+    )?.length || 0);
 
-    const antallStillinger = kandidatlisteForEier?.antallStillinger;
-    const besatteStillinger = antallKandidaterSomHarFåttJobb;
+    const antallStillinger = kandidatlisteForEier.data?.antallStillinger;
 
-    return antallStillinger === besatteStillinger;
-  }
+    setVisFullførStillingModal(antallStillinger === antallKandidaterSomHarFåttJobb);
+  };
 
   if (actionMenu) {
     return (
@@ -59,10 +65,7 @@ const RegistrerFåttJobbenKnapp: FC<RegistrerFåttJobbenKnappProps> = ({
         variant='secondary'
         disabled={lukketKandidatliste}
         icon={<ClipboardCheckmarkIcon />}
-        onClick={() => {
-          endreUtfallForKandidat(KandidatutfallTyper.FATT_JOBBEN)
-          setVisFullførStillingModal(sjekkOmAlleStillingerBesatt)
-        }}
+        onClick={() => håndterKnappetrykk()}
         loading={loading}
       >
         Registrer fått jobben
