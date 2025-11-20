552432dbaf592b0c892f81450fa2f6539626f443
Erlend Wiklem <admin@wiklem.no>
2025-11-20 11:20:58 +0100

    Anker element på Finn stilling og Finn kandidater

--- components/lenke-kort/LenkeKortMedIkon.tsx
+++ components/lenke-kort/LenkeKortMedIkon.tsx
@@ -1,10 +1,12 @@
+import { RekbisError } from '@/util/rekbisError';
 import { Box, LinkCard, Loader } from '@navikt/ds-react';
 import * as React from 'react';
 
 export interface LenkeKortMedIkonProps {
   tittel: string;
   beskrivelse?: string;
-  onClick: () => void;
+  href?: string;
+  onClick?: () => void;
   ikon: React.ReactElement<{ className?: string }> | string;
   loading?: boolean;
 }
@@ -12,10 +14,14 @@ export interface LenkeKortMedIkonProps {
 export default function LenkeKortMedIkon({
   tittel,
   beskrivelse,
-  onClick,
+  href,
   ikon,
   loading,
+  onClick,
 }: LenkeKortMedIkonProps) {
+  if (!href && !onClick) {
+    throw new RekbisError('LenkeKortMedIkon må få href eller onClick');
+  }
   return (
     <LinkCard data-color='accent'>
       <Box
@@ -23,6 +29,7 @@ export default function LenkeKortMedIkon({
         borderRadius='12'
         padding='space-8'
         style={{ backgroundColor: 'var(--ax-bg-moderateA)' }}
+        className='cursor-pointer'
       >
         <LinkCard.Icon>
           {loading ? (
@@ -39,9 +46,13 @@ export default function LenkeKortMedIkon({
         </LinkCard.Icon>
       </Box>
       <LinkCard.Title>
-        <LinkCard.Anchor asChild>
-          <div onClick={onClick}>{tittel}</div>
-        </LinkCard.Anchor>
+        {href ? (
+          <LinkCard.Anchor href={href}>{tittel}</LinkCard.Anchor>
+        ) : (
+          <LinkCard.Anchor asChild>
+            <div onClick={onClick}>{tittel}</div>
+          </LinkCard.Anchor>
+        )}
       </LinkCard.Title>
       <LinkCard.Description>{beskrivelse}</LinkCard.Description>
     </LinkCard>
