0d7199358840ad7ca13df3d45bbe55b845c3ba9e
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2025-11-21 08:39:56 +0100

    Småfiks

--- app/kandidat/_ui/IkonNavnAvatar.tsx
+++ app/kandidat/_ui/IkonNavnAvatar.tsx
@@ -2,7 +2,7 @@ import { BodyShort, Box } from '@navikt/ds-react';
 
 type IkonNavnAvatarProps =
   | { fulltNavn: string }
-  | { fornavn: string; etternavn: string; };
+  | { fornavn: string; etternavn: string };
 
 export default function IkonNavnAvatar(props: IkonNavnAvatarProps) {
   const hentInitialer = () => {
@@ -12,29 +12,31 @@ export default function IkonNavnAvatar(props: IkonNavnAvatarProps) {
         return navn.map((n) => n.charAt(0).toUpperCase()).join('');
       }
     } else {
-      if (props.fornavn && props.etternavn && props.fornavn.trim().length !== 0 && props.etternavn.trim().length !== 0) {
+      if (
+        props.fornavn &&
+        props.etternavn &&
+        props.fornavn.trim().length !== 0 &&
+        props.etternavn.trim().length !== 0
+      ) {
         return (
           props.fornavn.charAt(0).toUpperCase() +
           props.etternavn.charAt(0).toUpperCase()
         );
       }
     }
-  }
+  };
 
   return (
-      <Box.New
-        borderRadius={'full'}
-        background={'neutral-moderate'}
-        className='text-center py-2.5'
-        width={'40px'}
-        height={'40px'}
-      >
-        <BodyShort
-          textColor='subtle'
-          className='text-sm'
-        >
-          {hentInitialer()}
-        </BodyShort>
-      </Box.New>
+    <Box.New
+      borderRadius={'full'}
+      background={'neutral-moderate'}
+      className='py-2.5 text-center'
+      width={'40px'}
+      height={'40px'}
+    >
+      <BodyShort textColor='subtle' className='text-sm'>
+        {hentInitialer()}
+      </BodyShort>
+    </Box.New>
   );
 }
