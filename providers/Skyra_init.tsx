e1822c809f43faed00c8e07a0d3cf1ab7c6ba7cd
Erlend Wiklem <admin@wiklem.no>
2025-11-24 17:42:22 +0100

    Endret skyra-config-blokken til et vanlig <script> med dangerouslySetInnerHTML, så konfigurasjonen injiseres direkte uten Next Script-wrapper

--- providers/Skyra_init.tsx
+++ providers/Skyra_init.tsx
@@ -6,22 +6,21 @@ import Script from 'next/script';
 export default function SkyraInit() {
   return (
     <>
-      <Script
+      <script
         id='skyra-config'
-        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `window.SKYRA_CONFIG = { org: 'arbeids-og-velferdsetaten-nav' }`,
+        }}
         onError={(event) => {
           logger.error({ event }, 'Klarte ikke å laste skyra-config');
           window.dispatchEvent(
             new CustomEvent('skyra-status', { detail: 'error' }),
           );
         }}
-      >
-        {`window.SKYRA_CONFIG = { org: 'arbeids-og-velferdsetaten-nav' }`}
-      </Script>
+      />
 
       <Script
         src='https://survey.skyra.no/skyra-survey.js'
-        defer
         strategy='afterInteractive'
         onError={(event) => {
           logger.error({ event }, 'Klarte ikke å laste Skyra-scriptet');
