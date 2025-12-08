53ddef6180d38a381d84d4df447fd3d6e32ad0f0
haugse <sara.haugse@gmail.com>
2025-12-08 08:12:30 +0100

    Tillat arbeidsgiverrettet hvis pilotkontor

--- app/rekrutteringstreff/RekrutteringstreffPilotTilgang.tsx
+++ app/rekrutteringstreff/RekrutteringstreffPilotTilgang.tsx
@@ -13,17 +13,23 @@ export default function RekrutteringstreffPilotTilgang({
   skjulInnhold,
 }: RekrutteringstreffPilotTilgangProps) {
   const { valgtNavKontor, harRolle } = useApplikasjonContext();
-
-  if (getMiljø() !== Miljø.ProdGcp) {
-    return children;
-  }
-
-  if (
-    valgtNavKontor?.navKontor === '1001' || // Kristiansand
-    valgtNavKontor?.navKontor === '1621' || // Ørland
-    harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_UTVIKLER])
-  ) {
-    return children;
+  if(getMiljø() === Miljø.ProdGcp) {
+    if(harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_UTVIKLER])) {
+      return children
+    }
+  } else if(getMiljø() === Miljø.DevGcp) {
+    if(harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_UTVIKLER]) ||
+      //((harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET]) || harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET])) && (
+      ((harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET])) && (
+      valgtNavKontor?.navKontor === '1001' || // Kristiansand
+      valgtNavKontor?.navKontor === '0403' || // Hamar
+      valgtNavKontor?.navKontor === '0602' || // Drammen
+      valgtNavKontor?.navKontor === '0135' || // Råde
+      valgtNavKontor?.navKontor === '0211' || // Vestby
+      valgtNavKontor?.navKontor === '0300'    // Oslo
+    ))) {
+      return children
+    }
   }
 
   if (skjulInnhold) {
