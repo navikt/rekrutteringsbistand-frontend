b2fab565d4cf46d263893115b6ef92403c4a83e7
haugse <sara.haugse@gmail.com>
2025-12-08 11:36:27 +0100

    Legg til at rekrutteringstreff kan vises for jobbsøkerrettet innenfor pilot-kontor

--- app/rekrutteringstreff/RekrutteringstreffPilotTilgang.tsx
+++ app/rekrutteringstreff/RekrutteringstreffPilotTilgang.tsx
@@ -18,18 +18,17 @@ export default function RekrutteringstreffPilotTilgang({
       return children
     }
   } else if(getMiljø() === Miljø.DevGcp) {
-    if(harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_UTVIKLER]) ||
-      //((harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET]) || harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET])) && (
-      ((harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET])) && (
-      valgtNavKontor?.navKontor === '1001' || // Kristiansand
-      valgtNavKontor?.navKontor === '0403' || // Hamar
-      valgtNavKontor?.navKontor === '0602' || // Drammen
-      valgtNavKontor?.navKontor === '0135' || // Råde
-      valgtNavKontor?.navKontor === '0211' || // Vestby
-      valgtNavKontor?.navKontor === '0300'    // Oslo
-    ))) {
-      return children
-    }
+      if(harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_UTVIKLER]) ||
+        ((harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET]) || harRolle([Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET])) && (
+        valgtNavKontor?.navKontor === '1001' || // Kristiansand
+        valgtNavKontor?.navKontor === '0403' || // Hamar
+        //valgtNavKontor?.navKontor === '0602' || // Drammen
+        valgtNavKontor?.navKontor === '0135' || // Råde
+        valgtNavKontor?.navKontor === '0211' || // Vestby
+        valgtNavKontor?.navKontor === '0300'    // Oslo
+      ))) {
+        return children
+      }
   } else if(getMiljø() === Miljø.Lokalt) {
     return children
   }
