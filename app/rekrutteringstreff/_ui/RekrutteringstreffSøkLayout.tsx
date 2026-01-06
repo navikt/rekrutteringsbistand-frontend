fb258f8346b413878e1b1d44592836b1df7a1af7
Sara <43448582+haugse@users.noreply.github.com>
2026-01-06 11:57:24 +0100

    Legg til tilgangskontroll rundt knappen for nytt rekrutteringstreff (#257)
    * Legg til tilgangskontroll rundt knappen for nytt rekrutteringstreff
    
    Co-authored-by: Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
    
    * Endre lenke til logg for alarmer
    
    ---------
    
    Co-authored-by: Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>

--- app/rekrutteringstreff/_ui/RekrutteringstreffSøkLayout.tsx
+++ app/rekrutteringstreff/_ui/RekrutteringstreffSøkLayout.tsx
@@ -7,6 +7,8 @@ import {
 import PanelHeader from '@/components/layout/PanelHeader';
 import SideInnhold from '@/components/layout/SideInnhold';
 import SideLayout from '@/components/layout/SideLayout';
+import { Roller } from '@/components/tilgangskontroll/roller';
+import { TilgangskontrollForInnhold } from '@/components/tilgangskontroll/TilgangskontrollForInnhold';
 import { useApplikasjonContext } from '@/providers/ApplikasjonContext';
 import { useUmami } from '@/providers/UmamiContext';
 import { RekbisError } from '@/util/rekbisError';
@@ -54,9 +56,16 @@ const RekrutteringstreffSøkLayout: FC<RekrutteringstreffSøkLayoutProps> = ({
           <PanelHeader>
             <PanelHeader.Section
               actionsRight={
-                <Button onClick={handleOpprettRekrutteringstreff}>
-                  Nytt rekrutteringstreff
-                </Button>
+                <TilgangskontrollForInnhold
+                  skjulVarsel
+                  kreverEnAvRollene={[
+                    Roller.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
+                  ]}
+                >
+                  <Button onClick={handleOpprettRekrutteringstreff}>
+                    Nytt rekrutteringstreff
+                  </Button>
+                </TilgangskontrollForInnhold>
               }
             />
           </PanelHeader>
