153f847fe2b30272954c15e8ef746a8a78216430
Erlend Wiklem <admin@wiklem.no>
2026-01-09 10:59:07 +0100

    Endre synonym til jobbsøker

--- app/stilling/_ui/legg-til-kandidat/Synlighetsinfo.tsx
+++ app/stilling/_ui/legg-til-kandidat/Synlighetsinfo.tsx
@@ -51,7 +51,7 @@ const Synlighetsinfo: React.FC<SynlighetsinfoProps> = ({ fødselsnummer }) => {
             onClick={() => setVisSynlighetsEvaluering(true)}
             variant='secondary'
           >
-            Se hvorfor kandidaten ikke er synlig (punkt 1-4)
+            Se hvorfor jobbsøker ikke er synlig (punkt 1-4)
           </Button>
         </div>
       )}
