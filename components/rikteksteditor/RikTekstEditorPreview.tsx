206a787d6795e5ca6f4e0a634ab7d933f1ce8f14
Ken Ove Andersen <ken.ove.andersen@nav.no>
2026-02-05 12:01:26 +0100

    Fiks typo

--- components/rikteksteditor/RikTekstEditorPreview.tsx
+++ components/rikteksteditor/RikTekstEditorPreview.tsx
@@ -8,7 +8,7 @@ interface IRikTekstEditorPreview {
 const RikTekstEditorPreview: FC<IRikTekstEditorPreview> = ({ htmlContent }) => {
   return (
     <div
-      className='rikTestPreview max-w-none leading-normal text-gray-800 [&_li>p]:m-0 [&_li>p]:inline [&>ul]:list-inside [&>ul]:list-disc'
+      className='rikTekstPreview max-w-none leading-normal text-gray-800 [&_li>p]:m-0 [&_li>p]:inline [&>ul]:list-inside [&>ul]:list-disc'
       dangerouslySetInnerHTML={{ __html: htmlContent }}
     />
   );
