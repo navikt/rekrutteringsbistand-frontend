6860af75516f06f16755b212654b15a0f944d542
Erlend Wiklem <admin@wiklem.no>
2026-02-11 13:36:15 +0100

    wip

--- tests/formidling/vis-formidling.spec.ts
+++ tests/formidling/vis-formidling.spec.ts
@@ -1,11 +1,10 @@
 import { gotoApp } from '@/tests/gotoApp';
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 // Bruker arbeidsgiverrettet tilgang
 test.use({ storageState: 'tests/.auth/arbeigsgiverrettet.json' });
 
 test('Vis min etterregistrering', async ({ page }) => {
-  await gotoApp(page, '/etterregistrering/minFormidling');
+  await gotoApp(page, '/etterregistrering/etterregistreringformidling');
   await expect(page.getByRole('tab', { name: 'Om stillingen' })).toBeVisible();
-  await expect(page.getByRole('tab', { name: 'Jobbsøkere' })).toBeVisible();
 });
