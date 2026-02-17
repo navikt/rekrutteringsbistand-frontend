110d96da79c85e0e70f4969d60511af665801929
Erlend Wiklem <admin@wiklem.no>
2026-02-17 12:06:40 +0100

    ikke bruk custom url for tilbakemeldinger

--- app/api/bruker/tilbakemeldinger/[uuid]/route.ts
+++ app/api/bruker/tilbakemeldinger/[uuid]/route.ts
@@ -2,18 +2,10 @@ import { BrukerAPI } from '@/app/api/api-routes';
 import { proxyWithOBO } from '@/app/api/oboProxy';
 import { NextRequest } from 'next/server';
 
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: Promise<{ uuid: string }> },
-) {
-  const { uuid } = await params;
-  return proxyWithOBO(BrukerAPI, req, `/api/tilbakemeldinger/${uuid}`);
+export async function PUT(req: NextRequest) {
+  return proxyWithOBO(BrukerAPI, req);
 }
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: Promise<{ uuid: string }> },
-) {
-  const { uuid } = await params;
-  return proxyWithOBO(BrukerAPI, req, `/api/tilbakemeldinger/${uuid}`);
+export async function DELETE(req: NextRequest) {
+  return proxyWithOBO(BrukerAPI, req);
 }
