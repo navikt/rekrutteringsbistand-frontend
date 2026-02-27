329954100fe867b85d7d305d85cec44ebd329b95
Erlend Wiklem <admin@wiklem.no>
2026-02-27 12:16:26 +0100

    Set seed på jobbsøker hendelser

--- app/api/rekrutteringstreff/[...slug]/arbeidsgivere/arbeidsgiverHendelserMock.ts
+++ app/api/rekrutteringstreff/[...slug]/arbeidsgivere/arbeidsgiverHendelserMock.ts
@@ -1,11 +1,17 @@
 import { ArbeidsgiverHendelserDTO } from './useArbeidsgiverHendelser';
-import { faker } from '@faker-js/faker/locale/nb_NO';
+import { Faker, nb_NO } from '@faker-js/faker';
+
+const faker = new Faker({ locale: [nb_NO] });
 
 export const arbeidsgiverHendelserMock = (): ArbeidsgiverHendelserDTO => {
+  faker.seed(456);
+
+  const baseDate = new Date('2025-10-11T10:00:00+02:00');
+
   return [
     {
       id: faker.string.uuid(),
-      tidspunkt: new Date().toISOString(),
+      tidspunkt: baseDate.toISOString(),
       hendelsestype: 'OPPRETTET',
       opprettetAvAktørType: 'ARRANGØR',
       aktøridentifikasjon: 'testperson',
