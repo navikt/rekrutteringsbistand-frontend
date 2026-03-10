6ccc432d7fafebeb0ac5a2d303c1f70b9d4f723f
Erlend Wiklem <admin@wiklem.no>
2026-03-10 09:45:26 +0100

    Lint regel for process-env

---
+++ eslint/process-env-i-client.mjs
@@ -0,0 +1,39 @@
+const rule = {
+  meta: {
+    type: 'problem',
+    messages: {
+      noProcessEnv:
+        'Ikke bruk process.env i klientkode. Send verdien som prop fra en Server Component i stedet.',
+    },
+  },
+  create(context) {
+    let isClientFile = false;
+
+    return {
+      ExpressionStatement(node) {
+        if (
+          node.expression.type === 'Literal' &&
+          node.expression.value === 'use client'
+        ) {
+          isClientFile = true;
+        }
+      },
+      MemberExpression(node) {
+        if (!isClientFile) return;
+        if (
+          node.object.type === 'Identifier' &&
+          node.object.name === 'process' &&
+          node.property.name === 'env'
+        ) {
+          context.report({ node, messageId: 'noProcessEnv' });
+        }
+      },
+    };
+  },
+};
+
+export default {
+  rules: {
+    'no-process-env-in-client': rule,
+  },
+};
