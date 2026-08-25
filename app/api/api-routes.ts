1011980562cde3311cec3e386894adb29551d8d1
Tore Engvig <tore.engvig@nav.no>
2026-08-25 14:12:00 +0200

    Rette betingelser

--- app/api/api-routes.ts
+++ app/api/api-routes.ts
@@ -40,14 +40,14 @@ export const StatistikkAPI: Iroute = {
 
 export const ForespørselDelingAvCvAPI: Iroute = skalBrukeDelingAvCvAPI ? {
   api_route: '',
-  api_url: process.env.FORESPORSEL_OM_DELING_AV_CV_API ?? '',
-  internUrl: '/api/foresporsel-om-deling-av-cv',
-  scope: `api://${fss}.toi.foresporsel-om-deling-av-cv-api/.default`,
-} :   {
-  api_route: '',
   api_url: process.env.DELING_AV_CV_API ?? '',
   internUrl: '/api/foresporsel-om-deling-av-cv',
   scope: `api://${gcp}.toi.toi-deling-av-cv-api/.default`,
+} :   {
+  api_route: '',
+  api_url: process.env.FORESPORSEL_OM_DELING_AV_CV_API ?? '',
+  internUrl: '/api/foresporsel-om-deling-av-cv',
+  scope: `api://${fss}.toi.foresporsel-om-deling-av-cv-api/.default`,
 }
 
 ;
