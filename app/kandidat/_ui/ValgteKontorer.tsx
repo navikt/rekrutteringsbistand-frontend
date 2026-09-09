d2d6be70080b2758f1278a27a221558e16f8cdfc
Erlend Wiklem <admin@wiklem.no>
2026-09-09 10:13:07 +0200

    Bruk orgenhet istedenfor navn i kandidatsøk

--- app/kandidat/_ui/ValgteKontorer.tsx
+++ app/kandidat/_ui/ValgteKontorer.tsx
@@ -1,7 +1,15 @@
 import { useKontorSøk } from '@/app/api/kandidat-sok/useKontorSøk';
 import { useKandidatSøkFilterContext } from '@/app/kandidat/KandidaSokFilterContext';
+import navkontorer from '@/components/layout/modiadekoratør/enheter.json';
 import { UNSAFE_Combobox } from '@navikt/ds-react';
 import { FC, useRef, useState } from 'react';
+
+const finnEnhetId = (navn: string) =>
+  navkontorer.find((k) => k.navn.toLowerCase() === navn.toLowerCase())
+    ?.enhetId ?? navn;
+
+const finnNavn = (enhetId: string) =>
+  navkontorer.find((k) => k.enhetId === enhetId)?.navn ?? enhetId;
 
 const ValgteKontorer: FC = () => {
   const [søkeTekst, setSøkeTekst] = useState<string>('');
@@ -13,25 +21,30 @@ const ValgteKontorer: FC = () => {
   const { data, isLoading } = useKontorSøk(søkeTekst);
 
   const onOptionSelected = (option: string, isSelected: boolean) => {
+    const enhetId = finnEnhetId(option);
     if (isSelected) {
       setValgtKontor([
         ...(Array.isArray(valgtKontor) ? valgtKontor : []),
-        option,
+        enhetId,
       ]);
     } else {
       setValgtKontor(
         Array.isArray(valgtKontor)
-          ? valgtKontor.filter((o) => o !== option)
+          ? valgtKontor.filter((o) => o !== enhetId)
           : [],
       );
     }
   };
 
+  const selectedOptions = (Array.isArray(valgtKontor) ? valgtKontor : []).map(
+    finnNavn,
+  );
+
   return (
     <div ref={portalRef}>
       <UNSAFE_Combobox
         isLoading={isLoading}
-        selectedOptions={valgtKontor}
+        selectedOptions={selectedOptions}
         onToggleSelected={onOptionSelected}
         onChange={(val) => setSøkeTekst(val)}
         value={søkeTekst}
