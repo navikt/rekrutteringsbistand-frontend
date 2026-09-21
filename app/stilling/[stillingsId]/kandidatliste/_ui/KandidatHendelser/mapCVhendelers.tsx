5f7f36d119b44e6b8ea7c82714955dd97c77f98e
Tore Engvig <tengvig@Tore-sin-MacBook-Pro.local>
2026-09-21 14:16:08 +0200

    Inkluderer tilstanden SVARFRIST_UTLOPT

--- app/stilling/[stillingsId]/kandidatliste/_ui/KandidatHendelser/mapCVhendelers.tsx
+++ app/stilling/[stillingsId]/kandidatliste/_ui/KandidatHendelser/mapCVhendelers.tsx
@@ -83,7 +83,7 @@ export const mapCVHendele = (
         tekst: 'Kan ikke opprette forespørsel',
         ...defaultData,
       };
-    case TilstandPåForespørsel.AVBRUTT:
+    case TilstandPåForespørsel.AVBRUTT || TilstandPåForespørsel.SVARFRIST_UTLOPT:
       if (forespørsel.deltStatus === 'SENDT' && erFristUtløpt) {
         return {
           tag: (
