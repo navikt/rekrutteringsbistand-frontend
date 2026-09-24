a8b7b1c0e20d14ef179ddc1c3e29725d46df584a
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2026-09-24 13:37:47 +0200

    Gi fallback-tittel til sidepanelet i treff

--- app/rekrutteringstreff/[rekrutteringstreffId]/_ui/Rekrutteringstreff.tsx
+++ app/rekrutteringstreff/[rekrutteringstreffId]/_ui/Rekrutteringstreff.tsx
@@ -156,7 +156,7 @@ const Rekrutteringstreff: FC = () => {
               <Tabs value={fane} onChange={(val) => setFane(val)}>
                 <SideLayout
                   sidepanel={faneSidepanel.innhold ?? stegviserInnhold}
-                  sidepanelTittel={faneSidepanel.tittel ?? ''}
+                  sidepanelTittel={faneSidepanel.tittel ?? 'Sidepanel'}
                   sidepanelBredde='320px'
                   sidepanelBrekkpunkt={
                     fane === RekrutteringstreffTabs.JOBBSØKERE
